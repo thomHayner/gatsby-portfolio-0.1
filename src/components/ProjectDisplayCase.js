68fb79f4c9c367d56f024646e27ea9508692a825
thomHayner <thom.hayner@gmail.com>
2021-11-09 12:30:10 -0700

    code: init component

---
+++ src/components/ProjectDisplayCase.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const servicesBrochures = [
+  {
+    name: "Project Card #1",
+  },
+  {
+    name: "Project Card #2",
+  },
+  {
+    name: "Project Card #3",
+  },
+  {
+    name: "Project Card #4",
+  },
+  {
+    name: "Project Card #5",
+  },
+  {
+    name: "Project Card #6",
+  },
+]
+
+
+export default function ProjectDisplayCase() {
+  return (
+    <Container>
+      Project Display Case Placeholder
+    </Container>
+  )
+}
