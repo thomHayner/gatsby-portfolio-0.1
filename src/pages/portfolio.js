0fbbe573f641919882876ab51099b67bc7edd6a6
thomHayner <thom.hayner@gmail.com>
2021-11-09 12:21:02 -0700

    code: re-order page sections

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Layout from "../components/layout";
 import TechStackDisplayCase from "../components/TechStackDisplayCase";
+import ServicesPamphlet from "../components/ServicesPamphlet";
 
 export default function PortfolioPage() {
   return (
     <Layout>
-      <Container id="techStack" className="">
-        <TechStackDisplayCase/>
-        {/* data and then map the data */}
+      <Container id="services" className="">
       </Container>
       <hr/>
-      <Container id="services" className="">
-        {/* data and then map the data */}
+      <Container id="techStack" className="">
+        <TechStackDisplayCase/>
       </Container>
       <hr/>
       <Container id="projects" className="">
