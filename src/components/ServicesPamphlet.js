7fd82d671db4dc62d062de9fdc98ba95a88da3d0
thomHayner <thom.hayner@gmail.com>
2021-11-09 12:18:08 -0700

    init component

---
+++ src/components/ServicesPamphlet.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const servicesBrochures = [
+  {
+    name: "Available Service #1",
+  },
+  {
+    name: "Available Service #2",
+  },
+  {
+    name: "Available Service #3",
+  },
+  {
+    name: "Available Service #4",
+  },
+  {
+    name: "Available Service #5",
+  },
+  {
+    name: "Available Service #6",
+  },
+]
+
+
+export default function ServicesPamphlet() {
+  return (
+    <Container>
+      Services Pamphlet Placeholder
+    </Container>
+  )
+}
