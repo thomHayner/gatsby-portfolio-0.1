e1080d867098403f5e91adb4317610b5afef655d
thomHayner <thom.hayner@gmail.com>
2021-11-09 14:57:20 -0700

    code: dynamic render catalog of blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,73 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Layout from "../components/layout";
+import { StaticQuery, graphql } from 'gatsby';
 
 export default function BlogPage() {
   return (
-    <Layout>
-      <Container>
-        {/* <PostsTemplate> */}
-      </Container>
-    </Layout>
+    <StaticQuery
+      query={graphql`
+        query SiteQuery {
+          allStrapiArticle {
+            nodes {
+              author
+              title
+            }
+          }
+        }
+      `} 
+      render={data => (
+        <Layout>
+          <Container>
+            {data.allStrapiArticle.nodes.map((res, i) => (
+              <div>
+                <h4>{res.author}</h4>
+                <h5>{res.title}</h5>
+              </div>
+            ))}
+          </Container>
+        </Layout>
+      )}
+    />
   )
 }
+
+/*
+//
+
+<StaticQuery
+  query={graphql`
+    query SiteQuery {
+      allStrapiArticle {
+        nodes {
+          author
+          title
+        }
+      }
+    }
+  `}
+
+  render{data => (
+    <Layout>
+      <Container>
+        {data.allStrapiArticle.nodes.map((res, i) => (
+          <h4>{res.author}</h4>
+        ))}
+      </Container>
+    </Layout>
+  )}
+/>
+
+//
+//
+//
+
+<Layout>
+      <Container>
+        <div id="PostsTemplate" />
+  </Container>
+</Layout>
+
+//
+
+*/
