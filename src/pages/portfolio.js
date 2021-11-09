ef12be776f138c735ef20d190702c98cb9e753f3
thomHayner <thom.hayner@gmail.com>
2021-11-08 23:34:36 -0700

    add skeleton to page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Layout from "../components/layout";
 
 export default function PortfolioPage() {
   return (
     <Layout>
-      <div/>
+      <Container id="techStack" className=""></Container>
+      <hr/>
+      <Container id="services" className=""></Container>
+      <hr/>
+      <Container id="projects" className=""></Container>
     </Layout>
   )
 }
