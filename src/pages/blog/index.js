becc0f76c83ee66f9682583f793f427ea9573152
thomHayner <thom.hayner@gmail.com>
2021-11-10 03:05:31 -0700

    file: move and rename blog.js page file to /blog/index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Link, StaticQuery, graphql } from 'gatsby';
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 
 export default function BlogPage() {
   return (
     <StaticQuery
       query={graphql`
         query blogListQuery {
-          allStrapiArticle(limit: 10) {
+          allStrapiArticle(limit: 10, sort: {fields: id, order: DESC}) {
             nodes {
+              abstract
               author
+              publishedDate
               title
-              thumbnailCard {
-                abstract
-                title
-                id
-              }
             }
           }
         }
@@ -26,10 +23,13 @@ export default function BlogPage() {
           <Container>
             {data.allStrapiArticle.nodes.map(res => (
               <Link to={`/blog/${res.title.split(' ').join('-')}`}>
-                <h4>{res.author}</h4>
-                <h5>{res.title}</h5>
-                <h5>{console.log("CONSOLE LOGGEING" + res.thumbnailCard.title)}{res.thumbnailCard.title}</h5>
-                <p>{res.thumbnailCard.abstract}</p>
+                <h4>{res.title}</h4>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <h5>{res.author}</h5>
+                  <h5>. . . . . . . . . . .</h5>
+                  <h5>{res.publishedDate}</h5>
+                </div>
+                <p>{res.abstract}</p>
               </Link>
             ))}
           </Container>
