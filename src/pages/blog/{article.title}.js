bfec4698c8cf488fe72d5e28fbd99a40404e1f1b
thomHayner <thom.hayner@gmail.com>
2021-11-09 20:32:38 -0700

    code: add StaticQuery

--- src/pages/blog/{article.title}.js
+++ src/pages/blog/{article.title}.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { StaticQuery, graphql } from 'gatsby';
 
 export default function BlogArticle() {
   return (
-    <Layout>
-
-    </Layout>
+    // <div />
+    <StaticQuery 
+      query={graphql`
+        query blogPostQuery {
+          strapiArticle {
+            author
+            body
+            title
+            publishedDate
+            updated_at(fromNow: true)
+          }
+        }
+      `}
+      
+      render={
+        <Layout>
+        
+        </Layout>
+      }
+    />
   )
 }
