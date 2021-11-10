43cb6617b60d15ce1642324ce15b6470d59f2dba
thomHayner <thom.hayner@gmail.com>
2021-11-10 03:57:58 -0700

    code: add wireframe for layout

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Layout from '../components/layout';
 import { StaticQuery, graphql } from 'gatsby';
+import { Container } from 'react-bootstrap';
+import Layout from '../components/layout';
 
 export default function BlogArticle() {
   return (
-    // <div />
     <StaticQuery 
       query={graphql`
         query blogPostQuery {
@@ -18,11 +18,17 @@ export default function BlogArticle() {
         }
       `}
       
-      render={
+      render={data=> (
         <Layout>
-        
+          <Container>
+            <h3>{data.strapiArticle.title}</h3>
+            <h5>{data.strapiArticle.author}</h5>
+            <h5>{data.strapiArticle.publishedDate}</h5>
+            <h5>{data.strapiArticle.updated_at}</h5>
+            <p>{data.strapiArticle.body}</p>
+          </Container>
         </Layout>
-      }
+      )}
     />
   )
 }
