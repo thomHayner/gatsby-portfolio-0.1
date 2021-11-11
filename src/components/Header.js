3823b6753124ab37e458c37d30e1e22955ff94a6
thomHayner <thom.hayner@gmail.com>
2021-11-10 19:52:51 -0700

    code: change .svg to .png for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Nav, Container, Navbar } from 'react-bootstrap';
-import Logo from '../images/icon.svg';
+import Logo from '../images/icon.png';
 
 // This is just a data object for generating Nav Links dynamically
 const links = [
