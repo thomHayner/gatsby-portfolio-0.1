8ca3edf2983cc2ba88cc769dff2dabaa33f017cd
thomHayner <thom.hayner@gmail.com>
2021-11-11 07:57:12 -0700

    code: add div to control style

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,18 +5,20 @@ import { Container } from 'react-bootstrap';
 
 export default function Layout ({ children }) {
   return (
-    <Container>
-      <Header/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      {children}
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Footer/>
-    </Container>
+    <div style={{ backgroundColor: "#121212", color: "rgba(255, 255, 255, 0.87)" }} >
+      <Container>
+        <Header/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        {children}
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Footer/>
+      </Container>
+    </div>
   )
 }
