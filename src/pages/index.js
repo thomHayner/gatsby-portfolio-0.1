33ef0611c8b2554760fb9e489d7150043c0586e9
thomHayner <thom.hayner@gmail.com>
2021-11-10 18:45:31 -0700

    code: refactor page layout structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 // import { StaticQuery, graphQL } from 'gatsby';
+import { Container, Row } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout";
 
@@ -20,7 +21,18 @@ const IndexPage = () => {
   return (
     <main>
       <Layout>
-        <div><h1>WELCOME TO THE SITE!!!!!!</h1></div>
+        <Container>
+          <Row><br/></Row>
+          <Row><br/></Row>
+          <Row><hr/></Row>
+          <Row><h1>WELCOME TO THE SITE!!!!!!</h1></Row>
+          <Row><hr/></Row>
+          <Row><br/></Row>
+          {/* some content would go here */}
+          <Row><br/></Row>
+          <Row><br/></Row>
+          <Row><br/></Row>
+        </Container>
       </Layout>
     </main>
   )
