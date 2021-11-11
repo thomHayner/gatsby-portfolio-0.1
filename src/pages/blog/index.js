d63a2401afad311cb8fcf73e6d813cdffaaf03ca
thomHayner <thom.hayner@gmail.com>
2021-11-10 18:32:56 -0700

    code: refactor page layout structure

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { Link, StaticQuery, graphql } from 'gatsby';
 import Layout from "../../components/layout";
 
@@ -22,17 +22,33 @@ export default function BlogPage() {
       render={data => (
         <Layout>
           <Container>
-            {data.allStrapiArticle.nodes.map((res, i) => (
-              <Link to={`/blog/${res.title.split(' ').join('-')}`} key={i}>
-                <h4>{res.title}</h4>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                  <h5>{res.author}</h5>
-                  <h5>. . . . . . . . . . .</h5>
-                  <h5>{res.publishedDate}</h5>
-                </div>
-                <p>{res.abstract}</p>
-              </Link>
-            ))}
+            <Row><br/></Row>
+            <Row><br/></Row>
+            <Row><hr/></Row>
+            <Row><h1>Latest Posts</h1></Row>
+            <Row><hr/></Row>
+            <Row><br/></Row>
+            <Row>
+              {data.allStrapiArticle.nodes.map((res, i) => (
+              <Container>
+                <Row>
+                  <Link to={`/blog/${res.title.split(' ').join('-')}`} key={i}>
+                    <h4>{res.title}</h4>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                      <h5>{res.author}</h5>
+                      <h5>. . . . . . . . . . .</h5>
+                      <h5>{res.publishedDate}</h5>
+                    </div>
+                    <p>{res.abstract}</p>
+                  </Link>
+                </Row>
+                <Row><hr/></Row>
+              </Container>
+              ))}
+            </Row>
+            <Row><br/></Row>
+            <Row><br/></Row>
+            <Row><br/></Row>
           </Container>
         </Layout>
       )}
