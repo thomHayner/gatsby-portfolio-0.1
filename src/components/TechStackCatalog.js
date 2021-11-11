60c284d1063444f94e6275506dad1915cdefe112
thomHayner <thom.hayner@gmail.com>
2021-11-10 22:22:14 -0700

    code: cleanup

--- src/components/TechStackCatalog.js
+++ src/components/TechStackCatalog.js
@@ -1,40 +1,39 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import { Container, Row, Col, Tab, Tabs } from 'react-bootstrap';
+import { Container, Row, Tab, Tabs } from 'react-bootstrap';
 
 export default function TechStackCatalog() {
   return (
     <StaticQuery
-  query={graphql`
-    query techStackBadges {
-      allStrapiTechnology {
-        nodes {
-          name
-          description
-          img {
-            localFile {
-              url
+      query={graphql`
+        query techStackBadges {
+          allStrapiTechnology {
+            nodes {
+              name
+              description
+              img {
+                localFile {
+                  url
+                }
+              }
             }
           }
         }
-      }
-    }
-  `} 
+      `} 
 
-  render={data => (
-    <Container>
-      <Row>
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-          {data.allStrapiTechnology.nodes.map((node, i) => (
-            <Tab eventKey={node.name} title={ <img alt="" src={node.img.localFile.url} width="60" height="60" /> }>
-              {node.description}
-            </Tab>
-          ))}
-        </Tabs>
-      </Row>
-      <Row></Row>
-    </Container>
-  )}
-/>
+      render={data => (
+        <Container>
+          <Row>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+              {data.allStrapiTechnology.nodes.map((node, i) => (
+                <Tab eventKey={node.name} title={ <img alt="" src={node.img.localFile.url} width="60" height="60" /> }>
+                  {node.description}
+                </Tab>
+              ))}
+            </Tabs>
+          </Row>
+        </Container>
+      )}
+    />
   )
 }
