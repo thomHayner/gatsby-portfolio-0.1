37f1f73429d0afc5a3b59903885d49d065793f50
thomHayner <thom.hayner@gmail.com>
2021-11-11 00:25:02 -0700

    code: cleanup

--- src/components/ServicesPamphlet.js
+++ src/components/ServicesPamphlet.js
@@ -1,50 +1,6 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
-// const servicesBrochures = [
-//   {
-//     name: "Available Service #1",
-//     type: "Mobile App Development",
-//   },
-//   {
-//     name: "Available Service #2",
-//     type: "e-Commerce for retail, services, B2B or other use case",
-//   },
-//   {
-//     name: "Available Service #3",
-//     type: "Custom Workflow and Developer Tools",
-//   },
-//   {
-//     name: "Available Service #2",
-//     type: "Custom Software Applications",
-//   },
-//   {
-//     name: "Available Service #4",
-//     type: "UX / UI Improvements",
-//   },
-//   {
-//     name: "Available Service #6",
-//     type: "Technology and Software Integrations",
-//   },
-//   {
-//     name: "Available Service #5",
-//     type: "Database Management",
-//   },
-//   {
-//     name: "Available Service #6",
-//     type: "Cloud Computing",
-//   },
-//   {
-//     name: "Available Service #6",
-//     type: "MicroServices",
-//   },
-//   {
-//     name: "Available Service #6",
-//     type: "SaaS",
-//   },
-// ]
-
-
 export default function ServicesPamphlet() {
   return (
     <Container>
