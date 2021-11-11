21094c8603cc5233fe7d83ff179256f9d32e4bb5
thomHayner <thom.hayner@gmail.com>
2021-11-10 17:15:39 -0700

    file: chage filename to match Gatsby best practices

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,24 +3,25 @@ import { StaticQuery, graphql } from 'gatsby';
 import { Container } from 'react-bootstrap';
 import Layout from '../components/layout';
 
-export default function BlogArticle() {
+export default function BlogArticle(props) {
   return (
     <StaticQuery 
       query={graphql`
-        query blogPostQuery {
-          strapiArticle {
+        query blogPostQuery($id: Int) {
+          strapiArticle(id: {eq: "$id"}) {
             author
             body
             title
+            strapiId
             publishedDate
             updated_at(fromNow: true)
           }
-        }
+        }      
       `}
       
       render={data=> (
         <Layout>
-          <Container>
+          <Container data={props.data}>
             <h3>{data.strapiArticle.title}</h3>
             <h5>{data.strapiArticle.author}</h5>
             <h5>{data.strapiArticle.publishedDate}</h5>
