b48a42dc9fe5318df8bb1c97190a6b9adc4729db
thomHayner <thom.hayner@gmail.com>
2021-11-11 22:40:57 -0700

    code: remove wrapper container

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,13 +4,11 @@ import { Container, Card } from "react-bootstrap";
 
 export default function ServicesCard({ data }) {
   return(
-    <Container>
-        <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636" }} >
-          <Card.Title>{data.name}</Card.Title>
-          <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
-          <Card.Body>{data.description}</Card.Body>
-        </Card>
-    </Container>
+    <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636" }} >
+      <Card.Title>{data.name}</Card.Title>
+      <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
+      <Card.Body>{data.description}</Card.Body>
+    </Card>
   )
 }
 
