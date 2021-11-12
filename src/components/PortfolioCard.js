02ba41318061dc2cd388abeaa7503df1cb3b7087
thomHayner <thom.hayner@gmail.com>
2021-11-12 13:45:50 -0700

    code: change import to custom card

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,29 +1,42 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
-import { Container, Card } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
-export default function ServicesCard() {
+export default function PortfolioCard({ data }) {
   return(
-    <div/>
-    // <StaticQuery
-    //   query={graphql`
-    //     query portfolioCards {
-    //       allStrapiSkills {
-    //         nodes {
-    //           name
-    //           description
-    //         }
-    //       }
-    //     }
-    //   `}
-
-    //   render={data=> (
-    //     <Container>
-    //       <Card>
-
-    //       </Card>
-    //     </Container>
-    //   )}
-    // />
+      <Row>
+        <Col>
+          <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
+            <Card.Title>{data.name}</Card.Title>
+              <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
+            <Card.Body>{data.description}</Card.Body>
+          </Card>
+        </Col>
+      </Row>
   )
 }
+
+// export default function ServicesCard() {
+//   return(
+//     <div/>
+//     <StaticQuery
+//       query={graphql`
+//         query portfolioCards {
+//           allStrapiSkills {
+//             nodes {
+//               name
+//               description
+//             }
+//           }
+//         }
+//       `}
+
+//       render={data=> (
+//         <Container>
+//           <Card>
+
+//           </Card>
+//         </Container>
+//       )}
+//     />
+//   )
+// }
