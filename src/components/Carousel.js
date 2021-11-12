056efd0b09eff0b7f929d8eb647352dcc212c645
thomHayner <thom.hayner@gmail.com>
2021-11-12 01:58:26 -0700

    code: move query out of carousel and into wrapper layer

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import { StaticQuery, graphql } from 'gatsby';
 import { Container, Carousel } from 'react-bootstrap';
 import ServicesCard from './ServicesCard';
 
-export default function CardCarousel() {
+export default function CardCarousel({ data }) {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
@@ -11,35 +11,14 @@ export default function CardCarousel() {
   };
 
   return (
-    <StaticQuery
-      query={graphql`
-        query projectCardDeck { 
-          allStrapiSkill(sort: {fields: displayOrder, order: ASC}) { 
-            nodes { 
-              id 
-              name 
-              description 
-              img { 
-                localFile { 
-                  url 
-                } 
-              } 
-            } 
-          } 
-        } 
-      `} 
-
-      render={data => (
-        <Container>
-          <Carousel activeIndex={index} controls="false" onSelect={handleSelect()} fade >
-            {data.allStrapiSkill.nodes.map(node => (
-              <Carousel.Item>
-                <ServicesCard data={node} />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </Container>
-      )}
-    />
+    <Container>
+      <Carousel activeIndex={index} controls="false" onSelect={handleSelect()} fade >
+        {data.allStrapiSkill.nodes.map(node => (
+          <Carousel.Item>
+            <ServicesCard data={node} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   )
 }
