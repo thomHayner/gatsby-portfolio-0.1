e07ced103c06a8b84633d26c4a694cd6e2950962
thomHayner <thom.hayner@gmail.com>
2021-11-12 14:48:14 -0700

    code: adjust card dimensions

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 
 export default function PortfolioCard({ data }) {
   return(
-    <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
+    <Card style={{ width: "512px", height: "512px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
       <Card.Title>{data.name}</Card.Title>
         <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
       <Card.Body>{data.description}</Card.Body>
