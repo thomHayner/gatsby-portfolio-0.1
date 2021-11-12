c01922181abca261aae854511132dc28b52041ab
thomHayner <thom.hayner@gmail.com>
2021-11-11 22:43:49 -0700

    test: compare various layouts, options etc

--- src/components/TechStackCatalog.js
+++ src/components/TechStackCatalog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import { Container, Row, Tab, Tabs } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Tab, Tabs, TabPane } from 'react-bootstrap';
 
 export default function TechStackCatalog() {
   return (
@@ -27,12 +27,79 @@ export default function TechStackCatalog() {
           
             <Tabs defaultActiveKey="profile" id="uncontrolled-tab" className="mb-3" variant="pills" >
               {data.allStrapiTechnology.nodes.map((node, i) => (
-                <Tab key={node.id} eventKey={node.name} title={ <img alt="" src={node.img.localFile.url} width="60" height="60" /> }>
+                <TabPane key={node.id} eventKey={node.name} title={ <img alt="" src={node.img.localFile.url} width="60" height="60" /> }>
                   {node.description}
-                </Tab>
+                </TabPane>
               ))}
             </Tabs>
-          
+          {/* // */}
+            <Tab.Container>
+              <Nav variant="pills" className="flex-row">
+              {data.allStrapiTechnology.nodes.map((node, i) => (
+                <Nav.Item>
+                  <Nav.Link eventKey={node.name}>
+                    <img alt="" src={node.img.localFile.url} width="60" height="60" />
+                  </Nav.Link>
+                </Nav.Item>
+              ))}
+              </Nav>
+            </Tab.Container>
+          {/* // */}
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Row>
+                <Col>
+                
+                
+                </Col>
+                <Col sm={3}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second" style={{ backgroundColor: "rgba(255,255,255,.6)" }} >Tab 2</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+                <Col sm={3}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="fourth">Tab 4</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+                {/* <Col sm={9}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                      What 1
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      Who 2
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      Which 3
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="fourth">
+                      Where 4
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col> */}
+                <Col sm={3}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="fifth">Tab 5</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="sixth">Tab 6</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+              </Row>
+            </Tab.Container>
+
         </Container>
       )}
     />
