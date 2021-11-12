6ffbb4b2b1c2af62e8ebacf216031f4acda8999e
thomHayner <thom.hayner@gmail.com>
2021-11-12 13:50:06 -0700

    code: cleanup

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default function PortfolioCard({ data }) {
   return(
-    <Row>
-      <Col>
-        <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
-          <Card.Title>{data.name}</Card.Title>
-            <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
-          <Card.Body>{data.description}</Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <Card style={{ width: "224px", height: "400px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
+      <Card.Title>{data.name}</Card.Title>
+        <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
+      <Card.Body>{data.description}</Card.Body>
+    </Card>
   )
 }
 
