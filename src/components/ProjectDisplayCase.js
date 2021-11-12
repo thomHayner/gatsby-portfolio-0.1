f10b0aa9ffef52cfa423b221b22505b0f45f3e8a
thomHayner <thom.hayner@gmail.com>
2021-11-12 01:14:49 -0700

    code: cleanup unused

--- src/components/ProjectDisplayCase.js
+++ src/components/ProjectDisplayCase.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-// const servicesBrochures = [
-//   {
-//     name: "Project Card #1",
-//   },
-//   {
-//     name: "Project Card #2",
-//   },
-//   {
-//     name: "Project Card #3",
-//   },
-//   {
-//     name: "Project Card #4",
-//   },
-//   {
-//     name: "Project Card #5",
-//   },
-//   {
-//     name: "Project Card #6",
-//   },
-// ]
-
-
 export default function ProjectDisplayCase() {
   return (
     <Container>
