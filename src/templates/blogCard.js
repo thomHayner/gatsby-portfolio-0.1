9210f8e6426314b79c556f11d8516c637297daaf
thomHayner <thom.hayner@gmail.com>
2021-11-12 15:14:07 -0700

    code: layout card

--- src/templates/blogCard.js
+++ src/templates/blogCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
-export default function BlogCard() {
+export default function BlogCard({ data }) {
   return (
-    <div />
+    <Card className="bg-dark">
+      <Card.Title>{data.title}</Card.Title>
+      <Card.Subtitle>{data.publishedDate}</Card.Subtitle>
+      <Card.Body>{data.abstract}</Card.Body>
+    </Card>
   )
 }
