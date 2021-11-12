68f03ed2ffb4773312f82c96cc154bb722226678
thomHayner <thom.hayner@gmail.com>
2021-11-11 19:51:18 -0700

    add height attribute to make page fill the screen

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 
 export default function Layout ({ children }) {
   return (
-    <div style={{ backgroundColor: "#121212", color: "rgba(255, 255, 255, 0.87)" }} >
+    <div style={{ backgroundColor: "#121212", color: "rgba(255, 255, 255, 0.87)", height: "100vh" }} >
       <Container>
         <Header/>
         <br/>
