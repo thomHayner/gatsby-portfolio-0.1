59f679f740aa280766d5dea3dd48038fa8ef4de0
thomHayner <thom.hayner@gmail.com>
2021-11-14 14:42:40 -0700

    code: adjust styles

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -3,8 +3,8 @@ import { Container, Card } from "react-bootstrap";
 
 export default function ServicesCard({ data }) {
   return(
-    <Card className="m-3 p-5 border-dark bg-secondary" style={{ minWidth: "275px", minHeight: "150px" /* , backgroundColor: "#363636" */ }} >
-      <Container className="" style={{ height: "150px", width: "275px" }} >
+    <Card className="m-3 p-3 border-dark bg-secondary" style={{ minWidth: "325px", minHeight: "175px" /* , backgroundColor: "#363636" */ }} >
+      <Container className="m-1" style={{ minHeight: "150px", minWidth: "315px" }} >
       <Card.Title className="text-warning" >{data.title}</Card.Title>
       <Card.Body className="p-0" >{data.text}</Card.Body>
       </Container>
