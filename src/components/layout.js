53f9f8c9b2bd97c5963aab53eabd47b7b95c9fe5
thomHayner <thom.hayner@gmail.com>
2021-11-13 21:05:41 -0700

    code: remove unnecessary wrapper

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Container } from 'react-bootstrap';
 
 export default function Layout ({ children }) {
   return (
     <div style={{ backgroundColor: "rgba(18,18,18, .97)", backgroundSize: "cover", color: "rgba(255, 255, 255, 0.87)", minHeight: "100vh" }} >
-      <Container>
         <Header/>
         <br/>
         <br/>
@@ -18,7 +16,6 @@ export default function Layout ({ children }) {
         <br/>
         <br/>
         <Footer/>
-      </Container>
     </div>
   )
 }
