2d111b3052a7d94ec1a56f9a4f3da4d12656ca8c
thomHayner <thom.hayner@gmail.com>
2021-11-14 02:19:26 -0700

    code: refactor for content-less template

--- src/components/TechStackPamphlet.js
+++ src/components/TechStackPamphlet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import { Container, Row, Col, Nav, Tab, Figure } from 'react-bootstrap';
+import { Container, Row, Col, Figure } from 'react-bootstrap';
 
 // Make it collapse into even rows of icons
 // Maybe add a text description, one or two paragraphs, or maybe have it cycle through highlighting different stacks
@@ -9,63 +9,51 @@ export default function TechStackPamphlet() {
   return (
     <StaticQuery
       query={graphql`
-        query techStackBadges {
-          allStrapiTechnology(sort: {fields: displayOrder, order: ASC}, limit: 8) {
-            nodes {
-              id
-              name
-              description
-              img {
-                localFile {
-                  url
-                }
-              }
+      query techStackPamphlet {
+        strapiTechStackPamphlet {
+          id
+          title
+          subTitle
+          techBulletPoint {
+            id
+            text
+            title
+          }
+          devIcons {
+            localFile {
+              url
             }
           }
         }
+      }
       `} 
 
       render={data => (
         <Container className="m-3" >
           <Row>
             <Col className="d-flex justify-content-center" >
-              <h1>Technologies</h1>
+              <h1>{data.strapiTechStackPamphlet.title}</h1>
             </Col>
           </Row>
           <Row>
             <Col className="d-flex justify-content-center mb-3" >
-              <h5>How would you like it done?</h5>
+              <h5>{data.strapiTechStackPamphlet.subTitle}</h5>
             </Col>
           </Row>
           <Row>
             <Col>
-              <Row>
-                <h5>Serverless</h5>
-                <p>Products designed for cloud hosted services.</p>
-              </Row>
-              <Row>
-                <h5>Scalable</h5>
-                <p>Ready to grow with you.</p>
-              </Row>
-              <Row>
-                <h5>Simple</h5>
-                <p>Automated updates and continuous development integration.</p>
-              </Row>
+              {data.strapiTechStackPamphlet.techBulletPoint.map(bulletPoint => (
+                <Row key={bulletPoint.id} >
+                  <h5>{bulletPoint.title}</h5>
+                  <p>{bulletPoint.text}</p>
+                </Row>
+              ))}
             </Col>
             <Col className="d-flex justify-content-center" >
-                  {data.allStrapiTechnology.nodes.map((node, i) => (
-                    <Figure className="m-3" >
-                      <Figure.Image
-                        width={60}
-                        height={60}
-                        alt="60x60"
-                        src={node.img.localFile.url}
-                      />
-                      <Figure.Caption>
-                        {node.name}
-                      </Figure.Caption>
-                    {/* <img alt="" src={node.img.localFile.url} width="60rem" height="60rem" className="m-3" /> */}
-                    </Figure>
+                  {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
+                    <Container>
+                      <img alt="" src={devIcon.localFile.url} width="60rem" height="60rem" className="m-3" />
+                    </Container>
                   ))}
             </Col>
           </Row>
