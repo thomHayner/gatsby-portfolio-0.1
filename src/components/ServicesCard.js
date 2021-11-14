61d4b0e4d6c646f88f7526a42a70e9e384633ba2
thomHayner <thom.hayner@gmail.com>
2021-11-13 21:39:33 -0700

    code: adjust style, cleanup, refactor to follow color theme

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -3,8 +3,8 @@ import { Container, Card } from "react-bootstrap";
 
 export default function ServicesCard({ data }) {
   return(
-    <Card className="border-dark m-3 p-5" style={{ minWidth: "310px", height: "250px", backgroundColor: "#363636" }} >
-      <Container className="" style={{ width: "290px" }}>
+    <Card className="m-3 p-5 border-dark bg-secondary" style={{ minWidth: "275px", minHeight: "150px" /* , backgroundColor: "#363636" */ }} >
+      <Container className="" style={{ height: "150px", width: "275px" }}>
       <Card.Title className="text-warning" >{data.name}</Card.Title>
       <Card.Body className="p-0" >{data.description}</Card.Body>
       </Container>
