70c58d67e0dd594d77cc44dd482cbb51ffbd916d
thomHayner <thom.hayner@gmail.com>
2021-11-13 22:32:40 -0700

    code: cleanup

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,7 +4,7 @@ import { Container, Card } from "react-bootstrap";
 export default function ServicesCard({ data }) {
   return(
     <Card className="m-3 p-5 border-dark bg-secondary" style={{ minWidth: "275px", minHeight: "150px" /* , backgroundColor: "#363636" */ }} >
-      <Container className="" style={{ height: "150px", width: "275px" }}>
+      <Container className="" style={{ height: "150px", width: "275px" }} >
       <Card.Title className="text-warning" >{data.name}</Card.Title>
       <Card.Body className="p-0" >{data.description}</Card.Body>
       </Container>
