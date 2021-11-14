34977cc08f98bc59dcd47b6af2607e1a242d08ca
thomHayner <thom.hayner@gmail.com>
2021-11-14 01:50:22 -0700

    code: refactor for updated qeury

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -5,8 +5,8 @@ export default function ServicesCard({ data }) {
   return(
     <Card className="m-3 p-5 border-dark bg-secondary" style={{ minWidth: "275px", minHeight: "150px" /* , backgroundColor: "#363636" */ }} >
       <Container className="" style={{ height: "150px", width: "275px" }} >
-      <Card.Title className="text-warning" >{data.name}</Card.Title>
-      <Card.Body className="p-0" >{data.description}</Card.Body>
+      <Card.Title className="text-warning" >{data.title}</Card.Title>
+      <Card.Body className="p-0" >{data.text}</Card.Body>
       </Container>
     </Card>
   )
