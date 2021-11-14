9965a98a61b82df5fe1336e7ec2ce0cdf424b128
thomHayner <thom.hayner@gmail.com>
2021-11-13 21:14:16 -0700

    code: move section title from page to section, cleanup

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,17 +11,11 @@ export default function PortfolioPage() {
       <Container id="services" className="">
         <Row><ServicesPamphlet/></Row>
         <Row><br/></Row>
-        <Row><hr style={{ maxWidth: "200vw" }} /></Row>
+        <Row><hr/></Row>
         <Row><TechStackCatalog/></Row>
         {/* {certifications could possibly go here} */}
         <Row><br/></Row>
         <Row><hr/></Row>
-        <Row>
-          <Col className="d-flex justify-content-center" >
-            <h1>Projects</h1>
-          </Col>
-        </Row>
-        <Row><br/></Row>
         <Row><ProjectDisplayCase/></Row>
         {/* {testimonials could possibly go here} */}
       </Container>
