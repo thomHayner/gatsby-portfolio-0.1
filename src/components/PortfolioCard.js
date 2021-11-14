810e811e489ad7f0505defdd061436c89f211d4a
thomHayner <thom.hayner@gmail.com>
2021-11-13 23:17:07 -0700

    code: refactor for bootstrap style utils

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 
 export default function PortfolioCard({ data }) {
   return(
-    <Card style={{ width: "512px", height: "512px", alignItems: "center", backgroundColor: "#363636", justifyContent: "center" }} >
+    <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "512px", height: "512px" }} >
       <Card.Title>{data.name}</Card.Title>
         <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
       <Card.Body>{data.description}</Card.Body>
