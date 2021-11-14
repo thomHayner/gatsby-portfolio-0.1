ed8c46a90384532cd8e8fad16e573909b7cbaf94
thomHayner <thom.hayner@gmail.com>
2021-11-14 02:23:30 -0700

    code: cleanup

--- src/components/TechStackPamphlet.js
+++ src/components/TechStackPamphlet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import { Container, Row, Col, Figure } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Make it collapse into even rows of icons
 // Maybe add a text description, one or two paragraphs, or maybe have it cycle through highlighting different stacks
@@ -43,7 +43,7 @@ export default function TechStackPamphlet() {
           <Row>
             <Col>
               {data.strapiTechStackPamphlet.techBulletPoint.map(bulletPoint => (
-                <Row key={bulletPoint.id} >
+                <Row key={"bulletPoint_" + bulletPoint.id} className="m-3" >
                   <h5>{bulletPoint.title}</h5>
                   <p>{bulletPoint.text}</p>
                 </Row>
@@ -51,9 +51,7 @@ export default function TechStackPamphlet() {
             </Col>
             <Col className="d-flex justify-content-center" >
                   {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
-                    <Container>
-                      <img alt="" src={devIcon.localFile.url} width="60rem" height="60rem" className="m-3" />
-                    </Container>
+                      <img  key={"devIcon_" + devIcon.id} alt="" src={devIcon.localFile.url} width="60rem" height="60rem" className="m-3" />
                   ))}
             </Col>
           </Row>
