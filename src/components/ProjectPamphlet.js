f5cd259638113b6e716b8988cb08da2d9a97e0a2
thomHayner <thom.hayner@gmail.com>
2021-11-13 23:14:25 -0700

    code: build tab container

--- src/components/ProjectPamphlet.js
+++ src/components/ProjectPamphlet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import { Container, Col, Row, Nav, Tab } from 'react-bootstrap';
+import PortfolioCard from './PortfolioCard';
 
 
 export default function ProjectPamphlet() {
@@ -37,16 +38,29 @@ export default function ProjectPamphlet() {
           </Row>
           <Row>
             <Col className="d-flex justify-content-center" >
-              <Tab.Container>
-                <Nav variant="pills" className="flex-row">
-                  {data.allStrapiSkill.nodes.map((node, i) => (
-                    <Nav.Item>
-                      <Nav.Link eventKey={node.name}>
-                        <img alt="" src={node.img.localFile.url} width="60rem" height="60rem" />
-                      </Nav.Link>
-                    </Nav.Item>
-                  ))}
-                </Nav>
+              <Tab.Container id="" defaultActiveKey={data.allStrapiSkill.nodes[0].id} >
+                <Row>
+                  <Col sm={2} >
+                    <Nav variant="pills" className="flex-row">
+                      {data.allStrapiSkill.nodes.map((node, i) => (
+                        <Nav.Item>
+                          <Nav.Link eventKey={node.id}>
+                            <img alt="" src={node.img.localFile.url} width="60rem" height="60rem" />
+                          </Nav.Link>
+                        </Nav.Item>
+                      ))}
+                    </Nav>
+                  </Col>
+                  <Col sm={10}>
+                    <Tab.Content>
+                      {data.allStrapiSkill.nodes.map((node, i) => (
+                        <Tab.Pane eventKey={node.id}>
+                          <PortfolioCard data={node} />
+                        </Tab.Pane>
+                      ))}
+                    </Tab.Content>
+                  </Col>
+                </Row>
               </Tab.Container>
             </Col>
           </Row>
