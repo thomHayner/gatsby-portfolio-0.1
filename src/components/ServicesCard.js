0a7e083ab3d23856e3d6b1c9345d13608bd89fb5
thomHayner <thom.hayner@gmail.com>
2021-11-13 20:48:19 -0700

    code: cleanup

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { StaticQuery, graphql } from "gatsby";
 import { Container, Card } from "react-bootstrap";
 
 export default function ServicesCard({ data }) {
@@ -12,38 +11,3 @@ export default function ServicesCard({ data }) {
     </Card>
   )
 }
-
-{/* 
-  export default function ServicesCard({ data }) {
-    return(
-      <StaticQuery
-        query={graphql`
-          query skillCard($id: String!) { 
-            strapiSkill(id: {eq: "$id"}) {  
-              name 
-              description 
-              img { 
-                localFile { 
-                  url 
-                } 
-              } 
-              strapiId 
-            } 
-          }
-        `}
-
-        render={data=> (
-          <Container>
-              <Card style={{ width: "256px", height: "400px", alignItems: "center" }} >
-                <br/>
-                <Card.Title>{data.strapiSkill.name}</Card.Title>
-                <Card.Img src={data.strapiSkill.img.localFile.url} style={{ width: "200px", height: "200px" }} />
-                <Card.Body>{data.strapiSkill.description}</Card.Body>
-                {console.log("id: " + id)}
-              </Card>
-          </Container>
-        )}
-      /> 
-    )
-  }
-*/}
