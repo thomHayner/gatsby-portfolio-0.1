f42d9c2865797693a388af84cff1351618f213b5
thomHayner <thom.hayner@gmail.com>
2021-11-14 06:15:43 -0700

    code: adjust width to chase down bug

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 
 export default function ProjectCard({ data }) {
   return(
-    <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "512px", height: "512px" }} >
+    <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "400px", height: "512px" }} >
       <Card.Title>{data.name}</Card.Title>
         <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
       <Card.Body>{data.description}</Card.Body>
