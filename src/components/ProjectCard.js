1f8a160f100ee2bf845d60ece2809498a2d008dc
thomHayner <thom.hayner@gmail.com>
2021-11-15 10:54:03 -0700

    code: cleanup, readability

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,9 +4,13 @@ import { Card } from "react-bootstrap";
 export default function ProjectCard({ data }) {
   return(
     <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "400px", minWidth: "355px", height: "512px" }} >
-      <Card.Title>{data.name}</Card.Title>
-        <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
-      <Card.Body>{data.description}</Card.Body>
+      <Card.Title>
+        {data.name}
+      </Card.Title>
+      <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
+      <Card.Body>
+        {data.description}
+      </Card.Body>
     </Card>
   )
 }
