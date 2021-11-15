4128f8a51c9aa40fe4d5585f81b9a0e5ca01a95b
thomHayner <thom.hayner@gmail.com>
2021-11-15 06:56:18 -0700

    code: cleanup

--- src/components/TechStackPamphlet.js
+++ src/components/TechStackPamphlet.js
@@ -7,34 +7,34 @@ import DATA from '../assets/content/tech-stack-pamphlet.json'
 
 export default function TechStackPamphlet() {
   return (
-        <Container className="m-3" >
-          <Row>
-            <Col className="d-flex justify-content-center" >
-              <h1>{DATA.title}</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex justify-content-center mb-3" >
-              <h5>{DATA.subTitle}</h5>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="col-xl-4 col-lg-4 col-12" >
-              {DATA.techBulletPoint.map(bulletPoint => (
-                <Row key={"bulletPoint_" + bulletPoint.id} className="m-3" >
-                  <h5>{bulletPoint.title}</h5>
-                  <p>{bulletPoint.text}</p>
-                </Row>
-              ))}
-            </Col>
-            <Col className="col-xl-8 col-lg-8 col-12 d-flex justify-content-center flex-wrap" >
-              {/* {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
-                // <Card  key={"devIcon_" + devIcon.id} className="bg-success " >
-                  <img key={"devIcon_" + devIcon.id} alt="" src={devIcon.localFile.url} width="75rem" height="75rem" className="m-3" />
-                // </Card>
-              ))} */}
-            </Col>
-          </Row>
-        </Container>
+    <Container className="m-3" >
+      <Row>
+        <Col className="d-flex justify-content-center" >
+          <h1>{DATA.title}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center mb-3" >
+          <h5>{DATA.subTitle}</h5>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="col-xl-4 col-lg-4 col-12" >
+          {DATA.techBulletPoint.map(bulletPoint => (
+            <Row key={"bulletPoint_" + bulletPoint.id} className="m-3" >
+              <h5>{bulletPoint.title}</h5>
+              <p>{bulletPoint.text}</p>
+            </Row>
+          ))}
+        </Col>
+        <Col className="col-xl-8 col-lg-8 col-12 d-flex justify-content-center flex-wrap" >
+          {/* {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
+            // <Card  key={"devIcon_" + devIcon.id} className="bg-success " >
+              <img key={"devIcon_" + devIcon.id} alt="" src={devIcon.localFile.url} width="75rem" height="75rem" className="m-3" />
+            // </Card>
+          ))} */}
+        </Col>
+      </Row>
+    </Container>
   )
 }
