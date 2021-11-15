b7465bc95c3109f978637207a7a670356ee7273c
thomHayner <thom.hayner@gmail.com>
2021-11-15 03:34:38 -0700

    npm install gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
+    `gatsby-transformer-remark`,
     // {
     //   resolve: `gatsby-source-strapi`,
     //   options: {
