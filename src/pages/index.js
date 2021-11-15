a4faae437fc7fc0101603af2c2dd10287d8a8f61
thomHayner <thom.hayner@gmail.com>
2021-11-15 06:15:10 -0700

    code: cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,32 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout";
 import PortfolioPage from './portfolio'
 
-// // styles
-// const codeStyles = {
-//   color: "#8A6534",
-// }
-
-// // data
-// const links = [
-//   {
-//     color: "#663399",
-//   },
-// ]
-
-// markup
 const IndexPage = () => {
   return (
     <main>
-      <Layout />
-      {/* <PortfolioPage /> */}
-      {/* <Layout>
-        <Container>
-          <Row><h1>WELCOME TO THE SITE!!!!!!</h1></Row>
-          <Row><hr/></Row>
-          <Row><br/></Row> */}
-          {/* some content would go here
-        </Container>
-      </Layout> */}
+      <PortfolioPage />
     </main>
   )
 }
