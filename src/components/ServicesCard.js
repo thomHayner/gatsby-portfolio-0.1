e75706206051c54b82879f46254f3ca88778d61e
thomHayner <thom.hayner@gmail.com>
2021-11-15 10:53:56 -0700

    code: cleanup, readability

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,8 +4,12 @@ import { Card } from "react-bootstrap";
 export default function ServicesCard({ data }) {
   return(
     <Card className="border-dark bg-secondary m-3 p-4" style={{ minHeight: "180px", minWidth: "325px" }} >
-        <Card.Title className="text-warning" >{data.title}</Card.Title>
-        <Card.Body className="p-0" >{data.text}</Card.Body>
+      <Card.Title className="text-warning" >
+        {data.title}
+      </Card.Title>
+      <Card.Body className="p-0" >
+        {data.text}
+      </Card.Body>
     </Card>
   )
 }
