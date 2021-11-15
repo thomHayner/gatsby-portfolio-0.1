aee8b9f5e49b3f839eef9e43d6f228091038f23a
thomHayner <thom.hayner@gmail.com>
2021-11-15 06:54:40 -0700

    code: cleanup, adjust syle utils

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default function ServicesCard({ data }) {
   return(
-    <Card className="m-3 p-3 pt-4 border-dark bg-secondary" style={{ minWidth: "325px", minHeight: "175px" /* , backgroundColor: "#363636" */ }} >
-      <Container className="m-1" style={{ minHeight: "150px", minWidth: "315px" }} >
-      <Card.Title className="text-warning" >{data.title}</Card.Title>
-      <Card.Body className="p-0" >{data.text}</Card.Body>
-      </Container>
+    <Card className="border-dark bg-secondary m-3 p-4" style={{ minHeight: "180px", minWidth: "325px" }} >
+        <Card.Title className="text-warning" >{data.title}</Card.Title>
+        <Card.Body className="p-0" >{data.text}</Card.Body>
     </Card>
   )
 }
