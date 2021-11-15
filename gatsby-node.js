4a640755ccdb7a8dfed41cfb852d9376e3317d8a
thomHayner <thom.hayner@gmail.com>
2021-11-15 06:55:19 -0700

    temp: comment out for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { data } = await graphql(`
-    query blogPostQuery {
-      allStrapiArticle {
-        nodes {
-          title
-        }
-      }
-    }
-  `)
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { data } = await graphql(`
+//     query blogPostQuery {
+//       allStrapiArticle {
+//         nodes {
+//           title
+//         }
+//       }
+//     }
+//   `)
 
-  data.allStrapiArticle.nodes.forEach(node => {
-    const slug = node.title.split(' ').join('-')
-    actions.createPage({
-      path: '/blog/' + slug,
-      component: path.resolve('./src/templates/blog-post.js'),
-      context: { slug: slug },
-    })
-  })
+//   data.allStrapiArticle.nodes.forEach(node => {
+//     const slug = node.slug
+//     actions.createPage({
+//       path: '/blog/' + slug,
+//       component: path.resolve('./src/templates/blog-post.js'),
+//       context: { slug: slug },
+//     })
+//   })
 
-}
+// }
