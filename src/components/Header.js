5bb6aae57a08f11c2ac445318b92032342b7f68e
thomHayner <thom.hayner@gmail.com>
2021-11-15 10:54:44 -0700

    code: cleanup, syntax, refoactor for utils

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { Nav, Container, Navbar } from 'react-bootstrap';
 import Logo from '../assets/images/icon.png';
+import contentData from '../assets/content/contact-info.json';
 
 // This is just a data object for generating Nav Links dynamically
 const links = [
@@ -25,23 +26,22 @@ const links = [
 
 export default function Header() {
   return (
-    <Navbar fixed="top" style={{ backgroundColor: "#181818", borderBottom: "solid 1px", borderColor: "#000000" }} variant="dark" expand="md">
+    <Navbar style={{  }} className="bg-dark border-bottom border-info" variant="dark" fixed="top" expand="md" >
       <Container>
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="#" >
           <img 
             alt="" 
             src={Logo} 
             width="30" 
             height="30" 
           />
-          {``}
-          ThomasHayner
+          {contentData.displayName}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarResponsive" />
         <Navbar.Collapse id="navbarResponsive"  className="justify-content-end" >
           <Nav as="ul" >
             {links.map((link, i) => (
-              <Nav.Item as="li" key={"Link_"+ i}>
+              <Nav.Item as="li" key={"Link_"+ i} >
                 <Link 
                   to={link.to} 
                   className="nav-link" 
