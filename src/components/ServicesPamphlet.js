1782d6954239f5eadab67be5966cdedd4e3dcae5
thomHayner <thom.hayner@gmail.com>
2021-11-15 05:02:34 -0700

    code: refactor data source since content is static

--- src/components/ServicesPamphlet.js
+++ src/components/ServicesPamphlet.js
@@ -1,49 +1,30 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
 import { Container, Row, Col, CardGroup } from 'react-bootstrap';
 import ServicesCard from '../components/ServicesCard';
+import DATA from '../assets/portfolio-content/services-pamphlet.json';
 
 export default function ServicesPamphlet() {
   return (
-    <StaticQuery
-      query={graphql`
-        query servicesPamphlet {
-          strapiServicesPamphlet {
-            id
-            title
-            subTitle
-            services {
-              id
-              title
-              text
-            }
-          }
-        }
-      `} 
-
-      render={data => (
-        <Container className="m-3" >
-          <Row>
-            <Col className="d-flex justify-content-center" >
-              <h1>{data.strapiServicesPamphlet.title}</h1>
+    <Container className="m-3" >
+      <Row>
+        <Col className="d-flex justify-content-center" >
+          <h1>{DATA.title}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center mb-3" >
+          <h5>{DATA.subTitle}</h5>
+        </Col>
+      </Row>
+      <Row>
+        <CardGroup className="d-flex justify-content-center" >
+          {DATA.services.map(node => (
+            <Col key={node.id} >
+              <ServicesCard data={node} />
             </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex justify-content-center mb-3" >
-              <h5>{data.strapiServicesPamphlet.subTitle}</h5>
-            </Col>
-          </Row>
-          <Row>
-            <CardGroup className="d-flex justify-content-center" >
-              {data.strapiServicesPamphlet.services.map(node => (
-                <Col key={node.id} >
-                  <ServicesCard data={node} />
-                </Col>
-              ))}
-            </CardGroup>
-          </Row>
-        </Container>
-      )}
-    />
+          ))}
+        </CardGroup>
+      </Row>
+    </Container>
   )
 }
