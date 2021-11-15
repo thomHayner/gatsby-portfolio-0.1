1f5859f880dcd0552923cdd1571bc8ca22ea2e95
thomHayner <thom.hayner@gmail.com>
2021-11-15 05:08:37 -0700

    code: refactor data source since content is static

--- src/components/TechStackPamphlet.js
+++ src/components/TechStackPamphlet.js
@@ -1,47 +1,26 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import DATA from '../assets/content/tech-stack-pamphlet.json'
 
 // or maybe have it cycle through highlighting different stacks
+// decide whether to use devicons font or to import all of the files
 
 export default function TechStackPamphlet() {
   return (
-    <StaticQuery
-      query={graphql`
-      query techStackPamphlet {
-        strapiTechStackPamphlet {
-          id
-          title
-          subTitle
-          techBulletPoint {
-            id
-            text
-            title
-          }
-          devIcons {
-            localFile {
-              url
-            }
-          }
-        }
-      }
-      `} 
-
-      render={data => (
         <Container className="m-3" >
           <Row>
             <Col className="d-flex justify-content-center" >
-              <h1>{data.strapiTechStackPamphlet.title}</h1>
+              <h1>{DATA.title}</h1>
             </Col>
           </Row>
           <Row>
             <Col className="d-flex justify-content-center mb-3" >
-              <h5>{data.strapiTechStackPamphlet.subTitle}</h5>
+              <h5>{DATA.subTitle}</h5>
             </Col>
           </Row>
           <Row>
             <Col className="col-xl-4 col-lg-4 col-12" >
-              {data.strapiTechStackPamphlet.techBulletPoint.map(bulletPoint => (
+              {DATA.techBulletPoint.map(bulletPoint => (
                 <Row key={"bulletPoint_" + bulletPoint.id} className="m-3" >
                   <h5>{bulletPoint.title}</h5>
                   <p>{bulletPoint.text}</p>
@@ -49,15 +28,13 @@ export default function TechStackPamphlet() {
               ))}
             </Col>
             <Col className="col-xl-8 col-lg-8 col-12 d-flex justify-content-center flex-wrap" >
-              {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
+              {/* {data.strapiTechStackPamphlet.devIcons.map(devIcon => (
                 // <Card  key={"devIcon_" + devIcon.id} className="bg-success " >
                   <img key={"devIcon_" + devIcon.id} alt="" src={devIcon.localFile.url} width="75rem" height="75rem" className="m-3" />
                 // </Card>
-              ))}
+              ))} */}
             </Col>
           </Row>
         </Container>
-      )}
-    />
   )
 }
