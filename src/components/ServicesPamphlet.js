f1ad587f2581f8a638f48fea751ad103e7de27e5
thomHayner <thom.hayner@gmail.com>
2021-11-15 10:53:25 -0700

    code: cleanup, make JSON data name consistently 'contentData'

--- src/components/ServicesPamphlet.js
+++ src/components/ServicesPamphlet.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Container, Row, Col, CardGroup } from 'react-bootstrap';
 import ServicesCard from '../components/ServicesCard';
-import DATA from '../assets/content/services-pamphlet.json';
+import contentData from '../assets/content/services-pamphlet.json';
 
 export default function ServicesPamphlet() {
   return (
-    <Container className="" >
+    <Container>
       <Row>
         <Col className="d-flex justify-content-center" >
-          <h1>{DATA.title}</h1>
+          <h1>{contentData.title}</h1>
         </Col>
       </Row>
       <Row>
         <Col className="d-flex justify-content-center mb-3" >
-          <h5>{DATA.subTitle}</h5>
+          <h5>{contentData.subTitle}</h5>
         </Col>
       </Row>
       <Row>
         <CardGroup className="d-flex flex-wrap justify-content-center" >
-          {DATA.services.map(node => (
-            <Col key={node.id} className="col-lg-4 col-md-6 col-sm-12" >
+          {contentData.services.map(node => (
+            <Col key={`Services_${node.id}`} className="col-lg-4 col-md-6 col-sm-12" >
               <ServicesCard data={node} />
             </Col>
           ))}
