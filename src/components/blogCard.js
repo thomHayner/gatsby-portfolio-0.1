08f10c90771955ed8f90137042b9bf636476faa3
thomHayner <thom.hayner@gmail.com>
2021-11-16 17:39:37 -0700

    code: update props to match new  Markdown query

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -5,8 +5,8 @@ export default function BlogCard({ data }) {
   return (
     <Card className="bg-dark">
       <Card.Title>{data.title}</Card.Title>
-      <Card.Subtitle>{data.publishedDate}</Card.Subtitle>
-      <Card.Body>{data.abstract}</Card.Body>
+      <Card.Subtitle>{data.date}</Card.Subtitle>
+      {/* <Card.Body>{data.body}</Card.Body> // this needs to be limited to chars or it needs to use Fragment query method */}
     </Card>
   )
 }
