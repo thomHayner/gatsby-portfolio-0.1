0a7c0fbd5f9c0c8cc300351d5ec6f20da1ee02eb
thomHayner <thom.hayner@gmail.com>
2021-11-17 00:58:48 -0700

    code: cleanup to remove console warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import * as React from "react";
-// import { StaticQuery, graphQL } from 'gatsby';
-import { Container, Row } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from "../components/layout";
 import PortfolioPage from './portfolio'
 import '../index.scss';
 
