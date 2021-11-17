7cf08d57ca62e34e22aa978538110857d1bedc4f
thomHayner <thom.hayner@gmail.com>
2021-11-17 02:25:18 -0700

    code: comment out picture while still a placeholder

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,7 +7,7 @@ export default function ProjectCard({ data }) {
       <Card.Title>
         {data.name}
       </Card.Title>
-      <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} />
+      {/* <Card.Img src={data.img.localFile.url} style={{ width: "200px", height: "200px" }} /> */}
       <Card.Body>
         {data.description}
       </Card.Body>
