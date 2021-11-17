58ff2ac871f7880697e1d4bc0650d03196540a88
thomHayner <thom.hayner@gmail.com>
2021-11-16 19:12:44 -0700

    code: edit file for page generation per Gatsby Docs

---
+++ src/pages/blog/{MarkdownRemark.frontmatter__slug}.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Container, Row } from 'react-bootstrap';
+import Layout from '../../components/layout';
+
+export default function BlogArticleTemplate({ data }) {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark; 
+  return (
+    <Layout>
+      <Container>
+        <Row><img src={frontmatter.url} alt="" /></Row>
+        <Row><br/></Row>
+        <Row><hr/></Row>
+        <Row><h1>{frontmatter.title}</h1></Row>
+        <Row>{`On: ${frontmatter.date} By: ${frontmatter.author}`}</Row>
+        <Container className="" dangerouslySetInnerHTML={{ __html: html }} />
+        {/* <Row>{frontmatter.updated_at}</Row> */}
+        {/* <Row>{frontmatter.body}</Row> */}
+      </Container>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query oneBlogArticle($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date
+        slug
+        title
+      }
+    }
+  }
+`
