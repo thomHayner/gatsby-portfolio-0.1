bfb103d79d9fbedeedf93a04127b6d6a34f8abc8
thomHayner <thom.hayner@gmail.com>
2021-11-16 22:05:04 -0700

    code: cleanup

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -6,7 +6,6 @@ export default function BlogCard({ data }) {
     <Card className="bg-dark text-light">
       <Card.Title>{data.title}</Card.Title>
       <Card.Subtitle>{data.date}</Card.Subtitle>
-      {/* <Card.Body>{data.body}</Card.Body> // this needs to be limited to chars or it needs to use Fragment query method */}
     </Card>
   )
 }
