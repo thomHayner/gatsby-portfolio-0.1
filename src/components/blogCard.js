49e5c3fc748b8c79e1c678c99a445ac60c82a826
thomHayner <thom.hayner@gmail.com>
2021-11-16 21:36:45 -0700

    code: fix text color

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 
 export default function BlogCard({ data }) {
   return (
-    <Card className="bg-dark">
+    <Card className="bg-dark text-light">
       <Card.Title>{data.title}</Card.Title>
       <Card.Subtitle>{data.date}</Card.Subtitle>
       {/* <Card.Body>{data.body}</Card.Body> // this needs to be limited to chars or it needs to use Fragment query method */}
