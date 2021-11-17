d60a5fbb95ed0dc36122ec390860e963edbfbb17
thomHayner <thom.hayner@gmail.com>
2021-11-16 22:14:07 -0700

    code: update card layout

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -5,7 +5,11 @@ export default function BlogCard({ data }) {
   return (
     <Card className="bg-dark text-light">
       <Card.Title>{data.title}</Card.Title>
-      <Card.Subtitle>{data.date}</Card.Subtitle>
+      <Row>
+        <Card.Subtitle>{data.date}</Card.Subtitle>
+        <Card.Subtitle>{data.author}</Card.Subtitle>
+      </Row>
+      <Card.Body>{data.author}</Card.Body>
     </Card>
   )
 }
