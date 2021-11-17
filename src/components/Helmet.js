b1bdb8578dd99057a05d29fa3a0a0d3ce8f03da0
thomHayner <thom.hayner@gmail.com>
2021-11-17 12:36:17 -0700

    code: add and chop up remaining portion of example

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -15,4 +15,35 @@ const Helmet = ({ description, lang, meta, title }) => {
       }
     }`
   )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return(
+    <Helmet 
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+      ].concat(meta)}
+    />
+  )
 }
