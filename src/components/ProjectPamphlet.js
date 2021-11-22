071df05da2fe736189e2403719bb2ff718c88634
thomHayner <thom.hayner@gmail.com>
2021-11-21 21:07:48 -0700

    code: update component for new style template

--- src/components/ProjectPamphlet.js
+++ src/components/ProjectPamphlet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Col, Row, Nav, Tab } from 'react-bootstrap';
 import ProjectCard  from './ProjectCard';
+import contentData from '../assets/content/portfolio-pamphlet.json';
 
 // at "md" collapse down to vertical stack - done
 // at "sm" make it collapse into cards with overlays
@@ -13,12 +14,12 @@ export default function ProjectPamphlet() {
     <Container className="m-3" >
       <Row>
         <Col className="d-flex justify-content-center" >
-          <h1>Work</h1>
+          <h1>{contentData.title}</h1>
         </Col>
       </Row>
       <Row>
         <Col className="d-flex justify-content-center mb-3" >
-          <h5>Examples of various web technology uses</h5>
+          <h5>{contentData.subTitle}</h5>
         </Col>
       </Row>
       <Row>
@@ -28,16 +29,16 @@ export default function ProjectPamphlet() {
               <Col className="col-2" />
               <Col className="col-xl-1 col-lg-1 col-12" >
                 <Nav variant="pills" className="flex-row">
-                  {/* {data.allStrapiSkill.nodes.map((node, i) => (
+                  {/* {contentData.projects.map((project, i) => (
                     <Nav.Item>
-                      <Nav.Link eventKey={node.id}>
-                        <img alt="" src={node.img.localFile.url} width="60rem" height="60rem" />
+                      <Nav.Link eventKey={project.id}>
+                        <img alt={project.name} src={project.media.tabUrl} width="60rem" height="60rem" />
                       </Nav.Link>
                     </Nav.Item>
                   ))} */}
                   <Nav.Item>
                       <Nav.Link eventKey="1">
-                        <img alt="BattleshipPortfolioDisplay" src="" width="60rem" height="60rem" />
+                        <img alt={contentData.projects[0].name} src="" width="60rem" height="60rem" />
                       </Nav.Link>
                     </Nav.Item><Nav.Item>
                       <Nav.Link eventKey="2">
@@ -52,19 +53,19 @@ export default function ProjectPamphlet() {
               </Col>
               <Col className="col-xl-7 col-lg-7 col-12" >
                 <Tab.Content>
-                  {/* {data.allStrapiSkill.nodes.map((node, i) => (
-                    <Tab.Pane eventKey={node.id}>
-                      <ProjectCard data={node} />
+                  {/* {contentData.projects.map((project, i) => (
+                    <Tab.Pane eventKey={project.id}>
+                      <ProjectCard project={project} />
                     </Tab.Pane>
                   ))} */}
                   <Tab.Pane eventKey="1" >
-                    <ProjectCard data={{ name: "Battleship", description: "JavaScript, CSS, React" }} />
+                    <ProjectCard project={contentData.projects[0]} />
                   </Tab.Pane>
                   <Tab.Pane eventKey="2" >
-                    <ProjectCard data={{ name: "project 2", description: "Second Project To Display: Coming Soon!" }} />
+                    <ProjectCard project={contentData.projects[1]} />
                   </Tab.Pane>
                   <Tab.Pane eventKey="3" >
-                    <ProjectCard data={{ name: "project 3", description: "Third Project To Display: Coming Soon!" }} />
+                    <ProjectCard project={contentData.projects[2]} />
                   </Tab.Pane>
                 </Tab.Content>
               </Col>
