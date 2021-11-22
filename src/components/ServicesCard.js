2a9d0c65c0f82ba24d04e51b0cbb2c431f69f9a8
thomHayner <thom.hayner@gmail.com>
2021-11-21 22:20:22 -0700

    style: change text color

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap";
 export default function ServicesCard({ data }) {
   return(
     <Card className="bg-tertiary m-3 p-4" style={{ minHeight: "180px", minWidth: "325px" }} >
-      <Card.Title className="text-info" >
+      <Card.Title className="text-warning--" >
         {data.title}
       </Card.Title>
       <Card.Body className="p-0" >
