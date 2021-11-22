5647633ab529a045e260fb99bdaa3755c23e0f5c
thomHayner <thom.hayner@gmail.com>
2021-11-21 21:07:42 -0700

    code: update component for new style template

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-export default function ProjectCard({ data }) {
+export default function ProjectCard({ project }) {
   return(
-    <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "400px", minWidth: "355px", height: "512px" }} >
+    <Card className="text-white align-items-center justify-content-center bg-secondary" style={{ width: "900px", height: "720px", minHeight: "640px" }} >
       <Card.Title>
-        {data.name}
+        {project.name}
       </Card.Title>
-      <a href="http://battleship.thomashayner.com" >
-        <Card.Img src="" style={{ width: "200px", height: "200px" }} />
+      <a href={project.linkUrl} >
+        <Card.Img src={project.media.url} style={{  height: "480px" }} />
       </a>
       <Card.Body>
-        {data.description}
+        {project.description}
       </Card.Body>
     </Card>
   )
