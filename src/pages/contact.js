eaca6cf79abd2a519ab0717dc37362e7ec6e24d7
Thomas Hayner <thom.hayner@gmail.com>
2023-02-02 02:39:09 -0700

    separate out major page components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -74,6 +74,142 @@ export default function ContactPage() {
   };
   //// [END: GetForm Functions] ////
 
+  //// [START: Page Components] ////
+  const FormDisplay = () => (
+    <Form className="text-dark" >
+
+      <FloatingLabel
+        controlId="floatingInputContactName"
+        label="Name"
+        className="mb-3"
+      >
+        <Form.Control
+          aria-describedby='floatingInputContactName'
+          type="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel
+        controlId="floatingInputContactEmail"
+        label="Email"
+        className="mb-3"
+      >
+        <Form.Control
+          aria-describedby='floatingInputContactEmail'
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel
+        controlId="floatingInputContactSubject"
+        label="Subject"
+        className="mb-3"
+      >
+        <Form.Control
+          aria-describedby='floatingInputContactSubject'
+          type="subject"
+          placeholder="Subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+      </FloatingLabel>
+
+      <FloatingLabel
+        controlId="floatingTextareaContactMessage"
+        label="Message"
+        className="mb-3"
+      >
+        <Form.Control
+          aria-describedby='floatingTextareaContactMessage'
+          as="textarea"
+          style={{ height: '100px'}}
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </FloatingLabel>
+
+      <Button
+        variant="primary"
+        type="submit"
+        className="mb-3"
+        onClick={(e) => handleOnSubmit(e)}
+      >
+        Submit
+      </Button>
+
+    </Form>
+  );
+
+  const SubmittedDisplay = () => (
+    <Container>
+      <Row><p>{`Your message has been sent`}</p></Row>
+      <Row><p>{`We'll get back to you as soon as possible`}</p></Row>
+      <Button
+        variant="primary"
+        type="submit"
+        className="mb-3"
+        // onClick={(e) => handleOnSubmit(e)}
+      >
+        Go To Portfolio
+      </Button>
+      <Button
+        variant="primary"
+        type="submit"
+        className="mb-3"
+        onClick={(e) => handleClearSubmitted(e)}
+      >
+        New Message
+      </Button>
+    </Container>
+  );
+
+  const ReturnAddress = () => (
+    <address>
+      {contentData.contactAddressName ? <Row>{contentData.contactAddressName}</Row> : '' }
+      {contentData.contactAddress1 ? <Row>{contentData.contactAddress1}</Row> : '' }
+      {contentData.contactAddress2 ? <Row>{contentData.contactAddress2}</Row> : '' }
+      {contentData.contactAddress3 ? <Row>{contentData.contactAddress3}</Row> : '' }
+      {contentData.contactAddress4 ? <Row>{contentData.contactAddress4}</Row> : '' }
+      {contentData.contactAddress5 ? <Row>{contentData.contactAddress5}</Row> : '' }
+      {contentData.contactCity ?
+        contentData.contactState ?
+          contentData.contactCountry ?
+            <Row>{contentData.contactCity}, {contentData.contactState}, {contentData.contactCountry}</Row>
+          : 
+            <Row>{contentData.contactCity}, {contentData.contactState}</Row>
+        :
+          contentData.contactCountry ?
+            <Row>{contentData.contactCity}, {contentData.contactCountry}</Row>
+          :
+            <Row>{contentData.contactCity}</Row>
+        
+      : 
+        contentData.contactState ?
+          contentData.contactCountry ?
+            <Row>{contentData.contactState}, {contentData.contactCountry}</Row>
+          :
+            <Row>{contentData.contactState}</Row>
+      :
+        contentData.contactCountry ?
+        <Row>{contentData.contactCountry}</Row> : ''
+      }
+      {contentData.contactPostalCode ? <Row>{contentData.contactPostalCode}</Row> : '' }
+      {contentData.contactPhone ? <Row>{contentData.contactPhone}</Row> : '' }
+      {contentData.contactEmail ? <Row>{contentData.contactEmail}</Row> : '' }
+    </address>
+  );
+  //// [END: Page Components] ////
+
   return (
     <Layout>
       <Seo title="Contact" />
@@ -83,137 +219,10 @@ export default function ContactPage() {
         <Row><br/></Row>
         <Row>
           <Col>
-            {isSubmitted ?
-              <Container>
-                <Row><p>{`Your message has been sent`}</p></Row>
-                <Row><p>{`We'll get back to you as soon as possible`}</p></Row>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="mb-3"
-                  // onClick={(e) => handleOnSubmit(e)}
-                >
-                  Go To Portfolio
-                </Button>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="mb-3"
-                  onClick={(e) => handleClearSubmitted(e)}
-                >
-                  New Message
-                </Button>
-              </Container>
-            :
-            
-              <Form className="text-dark" >
-
-                <FloatingLabel
-                  controlId="floatingInputContactName"
-                  label="Name"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    aria-describedby='floatingInputContactName'
-                    type="name"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                  />
-                </FloatingLabel>
-
-                <FloatingLabel
-                  controlId="floatingInputContactEmail"
-                  label="Email"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    aria-describedby='floatingInputContactEmail'
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </FloatingLabel>
-
-                <FloatingLabel
-                  controlId="floatingInputContactSubject"
-                  label="Subject"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    aria-describedby='floatingInputContactSubject'
-                    type="subject"
-                    placeholder="Subject"
-                    value={subject}
-                    onChange={(e) => setSubject(e.target.value)}
-                  />
-                </FloatingLabel>
-
-                <FloatingLabel
-                  controlId="floatingTextareaContactMessage"
-                  label="Message"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    aria-describedby='floatingTextareaContactMessage'
-                    as="textarea"
-                    style={{ height: '100px'}}
-                    placeholder="Message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    required
-                  />
-                </FloatingLabel>
-
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="mb-3"
-                  onClick={(e) => handleOnSubmit(e)}
-                >
-                  Submit
-                </Button>
-
-              </Form>
-            }
+            {isSubmitted ? <SubmittedDisplay /> : <FormDisplay />}
           </Col>
           <Col className="ps-5 col-2" style={addressArea}>
-            <address>
-              {contentData.contactAddressName ? <Row>{contentData.contactAddressName}</Row> : '' }
-              {contentData.contactAddress1 ? <Row>{contentData.contactAddress1}</Row> : '' }
-              {contentData.contactAddress2 ? <Row>{contentData.contactAddress2}</Row> : '' }
-              {contentData.contactAddress3 ? <Row>{contentData.contactAddress3}</Row> : '' }
-              {contentData.contactAddress4 ? <Row>{contentData.contactAddress4}</Row> : '' }
-              {contentData.contactAddress5 ? <Row>{contentData.contactAddress5}</Row> : '' }
-              {contentData.contactCity ?
-                contentData.contactState ?
-                  contentData.contactCountry ?
-                    <Row>{contentData.contactCity}, {contentData.contactState}, {contentData.contactCountry}</Row>
-                  : 
-                    <Row>{contentData.contactCity}, {contentData.contactState}</Row>
-                :
-                  contentData.contactCountry ?
-                    <Row>{contentData.contactCity}, {contentData.contactCountry}</Row>
-                  :
-                    <Row>{contentData.contactCity}</Row>
-                
-              : 
-                contentData.contactState ?
-                  contentData.contactCountry ?
-                    <Row>{contentData.contactState}, {contentData.contactCountry}</Row>
-                  :
-                    <Row>{contentData.contactState}</Row>
-              :
-                contentData.contactCountry ?
-                <Row>{contentData.contactCountry}</Row> : ''
-              }
-              {contentData.contactPostalCode ? <Row>{contentData.contactPostalCode}</Row> : '' }
-              {contentData.contactPhone ? <Row>{contentData.contactPhone}</Row> : '' }
-              {contentData.contactEmail ? <Row>{contentData.contactEmail}</Row> : '' }
-            </address>
+            <ReturnAddress />
           </Col>
         </Row>
       </Container>
