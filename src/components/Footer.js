388663b9731241b3e38a5745ec95cfbeb09207f5
Thomas Hayner <thom.hayner@gmail.com>
2023-02-02 11:14:47 -0700

    cleanup, add target and rel to external links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Container, Navbar, Nav, Row } from 'react-bootstrap';
-import contentData from '../assets/content/contact-info.json';
+import contentData from '../assets/content/header-footer.json';
 import GitHub from '../assets/images/techStackIcons/github-original-wordmark.svg';
 import LinkedIn from '../assets/images/techStackIcons/linkedin-original.svg';
 import Email from '../assets/images/techStackIcons/EmailLogoSymbol.svg';
@@ -10,41 +10,45 @@ import Email from '../assets/images/techStackIcons/EmailLogoSymbol.svg';
 // sticky
 // max height
 // some sort of content in the center (or move social media buttons to center and add new content to right side)
+// privacy policy
 
 export default function Footer() {
   return (
-    <Navbar style={{  }} className="bg-primary border-top border-dark " variant="dark" fixed="bottom" >
-      <Container style={{  }} >
+    <Navbar style={{  }} className='bg-primary border-top border-dark' variant='dark' fixed='bottom'>
+      <Container style={{  }}>
           <Navbar.Brand>
-            <Row><h5>{`©${contentData.copyrightYear} ${contentData.displayName}`}</h5></Row>
-            <Row><h6>{`All Rights Reserved`}</h6></Row>
+            <Row><h6>{`©${contentData.copyrightYear} ${contentData.displayName}`}{` - All Rights Reserved`}</h6></Row>
           </Navbar.Brand>
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end" style={{  }} >
-            <Nav as="ul" >
-                <Nav.Item as="li" key={"FooterLink_"} >
-                  <Link 
-                    to={'/contact'} 
-                    className="nav-link" 
-                    activeClassName="active" 
-                    style={{ width: "55px", height: "55px"}} 
+          <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end' style={{  }}>
+            <Nav as='ul'>
+                <Nav.Item as='li' key={'FooterLink_'}>
+                  <Link
+                    to={'/contact'}
+                    className='nav-link'
+                    activeClassName='active'
+                    style={{ width: '45px', height: '45px'}}
                   >
                     <Email />
                   </Link>
                 </Nav.Item>
-                <Nav.Item as="li" >
+                <Nav.Item as='li'>
                   <a 
-                    href={contentData.socialMedia[1].url} 
-                    className="nav-link" 
-                    style={{ width: "55px", height: "55px" }} 
+                    href={contentData.socialMedia[1].url}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='nav-link'
+                    style={{ width: '45px', height: '45px' }}
                   >
                     <GitHub />
                   </a>
                 </Nav.Item>
-                <Nav.Item as="li" >
+                <Nav.Item as='li'>
                   <a 
-                    href={contentData.socialMedia[2].url} 
-                    className="nav-link" 
-                    style={{ width: "55px", height: "55px"}} 
+                    href={contentData.socialMedia[2].url}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='nav-link'
+                    style={{ width: '45px', height: '45px'}}
                   >
                     <LinkedIn />
                   </a>
