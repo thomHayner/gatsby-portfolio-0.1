1b4c5d668564937271fd732f0b580636b2d3c37b
Thomas Hayner <thom.hayner@gmail.com>
2023-02-02 11:15:16 -0700

    cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,50 +2,46 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { Nav, Container, Navbar } from 'react-bootstrap';
 import Logo from '../assets/images/icon.png';
-import contentData from '../assets/content/contact-info.json';
+import contentData from '../assets/content/header-footer.json';
 
 // This is just a data object for generating Nav Links dynamically
 const links = [
-  // {
-  //   text: "Home",
-  //   to: "/",
-  // },
   {
-    text: "Portfolio",
-    to: "/",
+    text: 'Portfolio',
+    to: '/',
   },
   {
-    text: "Blog",
-    to: "/blog",
+    text: 'Blog',
+    to: '/blog',
   },
   {
-    text: "Contact",
-    to: "/contact",
+    text: 'Contact',
+    to: '/contact',
   },
 ]
 
 export default function Header() {
   return (
-    <Navbar style={{  }} className="bg-primary border-bottom border-dark" variant="dark" fixed="top" expand="md" >
+    <Navbar style={{  }} className='bg-primary border-bottom border-dark' variant='dark' fixed='top' expand='md'>
       <Container>
-        <Navbar.Brand href="/" >
-          <img 
-            alt="" 
-            src={Logo} 
-            width="30" 
-            height="30" 
+        <Navbar.Brand href='/'>
+          <img
+            alt=''
+            src={Logo}
+            width='30'
+            height='30'
           />
           {contentData.displayName}
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarResponsive" />
-        <Navbar.Collapse id="navbarResponsive"  className="justify-content-end" >
-          <Nav as="ul" >
+        <Navbar.Toggle aria-controls='navbarResponsive' />
+        <Navbar.Collapse id='navbarResponsive'  className='justify-content-end'>
+          <Nav as='ul'>
             {links.map((link, i) => (
-              <Nav.Item as="li" key={"LinkH_"+ i} >
-                <Link 
-                  to={link.to} 
-                  className="nav-link" 
-                  activeClassName="active" 
+              <Nav.Item as='li' key={'LinkH_'+ i}>
+                <Link
+                  to={link.to}
+                  className='nav-link'
+                  activeClassName='active'
                 >
                   {link.text}
                 </Link>
@@ -55,4 +51,4 @@ export default function Header() {
       </Container>
     </Navbar>
   )
-}
+};
