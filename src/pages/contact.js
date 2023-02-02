9d3bf4df64d33e9c821e7e41868bcf5d35d503ca
Thomas Hayner <thom.hayner@gmail.com>
2023-02-02 02:37:00 -0700

    add isSubmitted component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,18 +19,26 @@ export default function ContactPage() {
   const [email, setEmail] = React.useState('');
   const [subject, setSubject] = React.useState('');
   const [message, setMessage] = React.useState('');
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
+
   const form = {
     name: name,
     email: email,
     subject: subject,
     message: message,
   };
+
   const handleFormReset = () => {
     setName('');
     setEmail('');
     setSubject('');
     setMessage('');
+    setIsSubmitted(true);
   }
+
+  const handleClearSubmitted = () => {
+    setIsSubmitted(false);
+  };
   //// [END: Form Values] ////
 
   //// [START: GetForm Functions] ////
@@ -39,7 +47,7 @@ export default function ContactPage() {
     status: null
   });
 
-  const handleServerResponse = (ok, msg, form) => {
+  const handleServerResponse = (ok, msg) => {
     setServerState({
       submitting: false,
       status: { ok, msg }
@@ -51,7 +59,6 @@ export default function ContactPage() {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    // const form = e.target;
     setServerState({ ...serverState, submitting: true });
     axios({
       method: "post",
@@ -76,78 +83,102 @@ export default function ContactPage() {
         <Row><br/></Row>
         <Row>
           <Col>
-            <Form className="text-dark" >
+            {isSubmitted ?
+              <Container>
+                <Row><p>{`Your message has been sent`}</p></Row>
+                <Row><p>{`We'll get back to you as soon as possible`}</p></Row>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mb-3"
+                  // onClick={(e) => handleOnSubmit(e)}
+                >
+                  Go To Portfolio
+                </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mb-3"
+                  onClick={(e) => handleClearSubmitted(e)}
+                >
+                  New Message
+                </Button>
+              </Container>
+            :
+            
+              <Form className="text-dark" >
 
-              <FloatingLabel
-                controlId="floatingInputContactName"
-                label="Name"
-                className="mb-3"
-              >
-                <Form.Control
-                  aria-describedby='floatingInputContactName'
-                  type="name"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
+                <FloatingLabel
+                  controlId="floatingInputContactName"
+                  label="Name"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    aria-describedby='floatingInputContactName'
+                    type="name"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </FloatingLabel>
 
-              <FloatingLabel
-                controlId="floatingInputContactEmail"
-                label="Email"
-                className="mb-3"
-              >
-                <Form.Control
-                  aria-describedby='floatingInputContactEmail'
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
+                <FloatingLabel
+                  controlId="floatingInputContactEmail"
+                  label="Email"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    aria-describedby='floatingInputContactEmail'
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </FloatingLabel>
 
-              <FloatingLabel
-                controlId="floatingInputContactSubject"
-                label="Subject"
-                className="mb-3"
-              >
-                <Form.Control
-                  aria-describedby='floatingInputContactSubject'
-                  type="subject"
-                  placeholder="Subject"
-                  value={subject}
-                  onChange={(e) => setSubject(e.target.value)}
-                />
-              </FloatingLabel>
+                <FloatingLabel
+                  controlId="floatingInputContactSubject"
+                  label="Subject"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    aria-describedby='floatingInputContactSubject'
+                    type="subject"
+                    placeholder="Subject"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                  />
+                </FloatingLabel>
 
-              <FloatingLabel
-                controlId="floatingTextareaContactMessage"
-                label="Message"
-                className="mb-3"
-              >
-                <Form.Control
-                  aria-describedby='floatingTextareaContactMessage'
-                  as="textarea"
-                  style={{ height: '100px'}}
-                  placeholder="Message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  required
-                />
-              </FloatingLabel>
+                <FloatingLabel
+                  controlId="floatingTextareaContactMessage"
+                  label="Message"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    aria-describedby='floatingTextareaContactMessage'
+                    as="textarea"
+                    style={{ height: '100px'}}
+                    placeholder="Message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                  />
+                </FloatingLabel>
 
-              <Button
-                variant="primary"
-                type="submit"
-                className="mb-3"
-                onClick={(e) => handleOnSubmit(e)}
-              >
-                Submit
-              </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mb-3"
+                  onClick={(e) => handleOnSubmit(e)}
+                >
+                  Submit
+                </Button>
 
-            </Form>
+              </Form>
+            }
           </Col>
           <Col className="ps-5 col-2" style={addressArea}>
             <address>
