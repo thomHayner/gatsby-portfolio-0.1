630619e7991f04d42d563f6006daa9d3f45371eb
Thomas Hayner <thom.hayner@gmail.com>
2023-02-02 10:06:16 -0700

    cleanup ReturnAddress section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -175,12 +175,13 @@ export default function ContactPage() {
 
   const ReturnAddress = () => (
     <address>
-      {contentData.contactAddressName ? <Row>{contentData.contactAddressName}</Row> : '' }
-      {contentData.contactAddress1 ? <Row>{contentData.contactAddress1}</Row> : '' }
-      {contentData.contactAddress2 ? <Row>{contentData.contactAddress2}</Row> : '' }
-      {contentData.contactAddress3 ? <Row>{contentData.contactAddress3}</Row> : '' }
-      {contentData.contactAddress4 ? <Row>{contentData.contactAddress4}</Row> : '' }
-      {contentData.contactAddress5 ? <Row>{contentData.contactAddress5}</Row> : '' }
+      {contentData.contactName ? <Row>{contentData.contactName}</Row> : <div /> }
+      {contentData.contactCompany ? <Row>{contentData.contactCompany}</Row> : <div /> }
+      {contentData.contactAddress1 ? <Row>{contentData.contactAddress1}</Row> : <div /> }
+      {contentData.contactAddress2 ? <Row>{contentData.contactAddress2}</Row> : <div /> }
+      {contentData.contactAddress3 ? <Row>{contentData.contactAddress3}</Row> : <div /> }
+      {contentData.contactAddress4 ? <Row>{contentData.contactAddress4}</Row> : <div /> }
+      {contentData.contactAddress5 ? <Row>{contentData.contactAddress5}</Row> : <div /> }
       {contentData.contactCity ?
         contentData.contactState ?
           contentData.contactCountry ?
@@ -201,11 +202,11 @@ export default function ContactPage() {
             <Row>{contentData.contactState}</Row>
       :
         contentData.contactCountry ?
-        <Row>{contentData.contactCountry}</Row> : ''
+        <Row>{contentData.contactCountry}</Row> : <div />
       }
-      {contentData.contactPostalCode ? <Row>{contentData.contactPostalCode}</Row> : '' }
-      {contentData.contactPhone ? <Row>{contentData.contactPhone}</Row> : '' }
-      {contentData.contactEmail ? <Row>{contentData.contactEmail}</Row> : '' }
+      {contentData.contactPostalCode ? <Row>{contentData.contactPostalCode}</Row> : <div /> }
+      {contentData.contactPhone ? <Row>{contentData.contactPhone}</Row> : <div /> }
+      {contentData.contactEmail ? <Row>{contentData.contactEmail}</Row> : <div /> }
     </address>
   );
   //// [END: Page Components] ////
