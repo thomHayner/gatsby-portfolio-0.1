069228e82f68d382fab985377e2b212520e4cd1c
Thomas Hayner <thom.hayner@gmail.com>
2023-02-03 17:50:05 -0700

    cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,14 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout ({ children }) {
-  // img tag just pings heroku app to start the free teir dyno.  Since heroku app is linked to this
+  // img tag just pings heroku app to start the free tier dyno.  Since heroku app is linked to this
   // domain, it is giving me a CORS error.  I might make a third app that acts as a middleware.
   // Have image make call to third site, have third site ping heroku app so that it starts the dyno.
   // Another option is to install the npm ping package or to look for a gatsby package.
   return (
-    <div className="bg-secondary text-light min-vh-100 " >
-      <img src="https://th-battleship-js-demo.herokuapp.com/" alt="nada" style={{ margin: "-5000px" }} />
-      <Header/>
+    <div className="bg-primary text-light min-vh-100 " >
+      {/* <img src="https://th-battleship-js-demo.herokuapp.com/" alt="nada" style={{ margin: "-5000px" }} /> */}
+      <Header />
       <br/>
       <br/>
       <br/>
@@ -20,7 +20,7 @@ export default function Layout ({ children }) {
       <br/>
       <br/>
       <br/>
-      <Footer/>
+      <Footer />
     </div>
   )
-}
+};
