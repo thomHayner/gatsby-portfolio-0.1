19e34d284b7a3631c5988ecea2980719ee818f74
Thomas Hayner <thom.hayner@gmail.com>
2023-02-04 15:36:58 -0700

    add check to fix error in gatsby-cloud build

--- src/utilities/useResizableWindow.js
+++ src/utilities/useResizableWindow.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
 
 export default function useResizableWindow() {
+  // Check if window is defined (so if in the browser or in node.js).
+  const isBrowser = typeof window !== "undefined";
+
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
     height: window.innerHeight
   });
+  
   const handleResize = () => {
-    setDimensions({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
+    if (isBrowser) {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    };
   };
+
   useEffect(() => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
