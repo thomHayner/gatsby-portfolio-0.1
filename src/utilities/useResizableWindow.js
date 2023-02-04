0143239f51103fb8ed70b4b87aa1a38f0c836d63
Thomas Hayner <thom.hayner@gmail.com>
2023-02-04 13:57:51 -0700

    implement resizable window hook

---
+++ src/utilities/useResizableWindow.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export default function useResizableWindow() {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  const handleResize = () => {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return dimensions;
+};
+
+// export default {
+//   useResizableWindow,
+// }
