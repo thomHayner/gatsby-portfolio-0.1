8c84c5a3d5dbd520f3f2fdedb2399aa63ac8e176
Thomas Hayner <thom.hayner@gmail.com>
2023-02-05 02:33:39 -0700

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,43 +33,43 @@ module.exports = {
         // Generate PWA icons and a favicon
         icon: `src/assets/images/icon.png`,
         // crossOrigin: `anonymous`,
-      //   localize: [
-      //     {
-      //       start_url: `/es/`,
-      //       lang: `es`,
-      //       name: `Tomás Hayner`,
-      //       short_name: `TomásHayner`,
-      //       description: `Portafolio de ingeniería de software de Tomás Hayner.`,
-      //     },
-      //     {
-      //       start_url: `/de/`,
-      //       lang: `de`,
-      //       name: `Thomas Hayner`,
-      //       short_name: `ThomasHayner`,
-      //       description: `Das Software Engineering-Portfolio von Thomas Hayner.`,
-      //     },
-      //     {
-      //       start_url: `/fr/`,
-      //       lang: `fr`,
-      //       name: `Thomas Hayner`,
-      //       short_name: `Thomas Hayner`,
-      //       description: `Portefeuille de génie logiciel de Thomas Hayner.`,
-      //     },
-      //     {
-      //       start_url: `/it/`,
-      //       lang: `it`,
-      //       name: `Tomaso Hayner`,
-      //       short_name: `TomasoHayner`,
-      //       description: `Portfolio di ingegneria del software di Tomaso Hayner.`,
-      //     },
-      //     {
-      //       start_url: `/pt/`,
-      //       lang: `pt`,
-      //       name: `Tomás Hayner`,
-      //       short_name: `TomásHayner`,
-      //       description: `Portfólio de Engenharia de Software de Tomás Hayner.`,
-      //     },
-      //   ],
+        // localize: [
+        //   {
+        //     start_url: `/es/`,
+        //     lang: `es`,
+        //     name: `Tomás Hayner`,
+        //     short_name: `TomásHayner`,
+        //     description: `Portafolio de ingeniería de software de Tomás Hayner.`,
+        //   },
+        //   {
+        //     start_url: `/de/`,
+        //     lang: `de`,
+        //     name: `Thomas Hayner`,
+        //     short_name: `ThomasHayner`,
+        //     description: `Das Software Engineering-Portfolio von Thomas Hayner.`,
+        //   },
+        //   {
+        //     start_url: `/fr/`,
+        //     lang: `fr`,
+        //     name: `Thomas Hayner`,
+        //     short_name: `Thomas Hayner`,
+        //     description: `Portefeuille de génie logiciel de Thomas Hayner.`,
+        //   },
+        //   {
+        //     start_url: `/it/`,
+        //     lang: `it`,
+        //     name: `Tomaso Hayner`,
+        //     short_name: `TomasoHayner`,
+        //     description: `Portfolio di ingegneria del software di Tomaso Hayner.`,
+        //   },
+        //   {
+        //     start_url: `/pt/`,
+        //     lang: `pt`,
+        //     name: `Tomás Hayner`,
+        //     short_name: `TomásHayner`,
+        //     description: `Portfólio de Engenharia de Software de Tomás Hayner.`,
+        //   },
+        // ],
       }
     },
     {
