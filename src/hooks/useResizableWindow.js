0b12e57c1b0a6a22c8736339e17bcbf5fd5ee2a5
Thomas Hayner <thom.hayner@gmail.com>
2023-02-05 12:55:17 -0700

    try ssr resize with 'browser-monads' package

--- src/hooks/useResizableWindow.js
+++ src/hooks/useResizableWindow.js
@@ -1,23 +1,50 @@
 import { useState, useEffect } from 'react';
-//// DEVNOTE:
-/* There is a useEffect in src/pages/index.js which forces a single resize 
-on load to make sure that this works wit SSR and builds on gatsby-cloud. */
+import { window } from 'browser-monads';
 
 export default function useResizableWindow() {
   const [dimensions, setDimensions] = useState({
-    width: typeof window !== 'undefined' ? window.innerWidth : 0,
-    height: typeof window !== 'undefined' ? window.innerHeight : 0
+    width: undefined,
+    height: undefined,
   });
-  const handleResize = () => {
-    setDimensions({
-      width: window.innerWidth,
-      height: window.innerHeight
-    })
-  };
+
   useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    };
     window.addEventListener('resize', handleResize);
+    handleResize();
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return dimensions;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//// DEVNOTE:
+/* There is a useEffect in src/pages/index.js which forces a single resize 
+on load to make sure that this works wit SSR and builds on gatsby-cloud. */
+
+// export default function useResizableWindow() {
+//   const [dimensions, setDimensions] = useState({
+//     width: typeof window !== 'undefined' ? window.innerWidth : 0,
+//     height: typeof window !== 'undefined' ? window.innerHeight : 0
+//   });
+//   const handleResize = () => {
+//     setDimensions({
+//       width: window.innerWidth,
+//       height: window.innerHeight
+//     })
+//   };
+//   useEffect(() => {
+//     window.addEventListener('resize', handleResize);
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
+
+//   return dimensions;
+// };
