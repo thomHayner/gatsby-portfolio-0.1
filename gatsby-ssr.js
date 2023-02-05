11759475542da0aec2929419fd97c7417c3044b2
Thomas Hayner <thom.hayner@gmail.com>
2023-02-05 03:56:14 -0700

    try setting window height/width in ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,8 @@ exports.onRenderBody = ({ setBodyAttributes, pathname, }) => {
   setBodyAttributes({
     style: {
       backgroundColor: '#0a192f',
+      minHeight: window.innerHeight,
+      minWidth: window.innerWidth,
     },
   });
 };
