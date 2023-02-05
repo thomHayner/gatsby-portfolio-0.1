c95f2410fb2950af431f93506d96f4e3f4cd79e4
Thomas Hayner <thom.hayner@gmail.com>
2023-02-05 12:46:40 -0700

    cleanup

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,7 @@
-const React = require("react");
-const { default: useResizableWindow } = require("./src/hooks/useResizableWindow");
-
 exports.onRenderBody = ({ setBodyAttributes, pathname, }) => {
   setBodyAttributes({
     style: {
       backgroundColor: '#0a192f',
-      minHeight: useResizableWindow.height,
-      minWidth: useResizableWindow.width,
     },
   });
 };
