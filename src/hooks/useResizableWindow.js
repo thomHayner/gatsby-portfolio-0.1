a1ce7508d16cfc9d044f45a459510e62ca648c42
Thomas Hayner <thom.hayner@gmail.com>
2023-02-05 13:57:31 -0700

    retest original hook without monads

--- src/hooks/useResizableWindow.js
+++ src/hooks/useResizableWindow.js
@@ -1,26 +1,26 @@
 import { useState, useEffect } from 'react';
-import { window } from 'browser-monads';
+// import { window } from 'browser-monads';
 
-export default function useResizableWindow() {
-  const [dimensions, setDimensions] = useState({
-    width: undefined,
-    height: undefined,
-  });
+// export default function useResizableWindow() {
+//   const [dimensions, setDimensions] = useState({
+//     width: undefined,
+//     height: undefined,
+//   });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight
-      })
-    };
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setDimensions({
+//         width: window.innerWidth,
+//         height: window.innerHeight
+//       })
+//     };
+//     window.addEventListener('resize', handleResize);
+//     handleResize();
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
 
-  return dimensions;
-};
+//   return dimensions;
+// };
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -30,21 +30,22 @@ export default function useResizableWindow() {
 /* There is a useEffect in src/pages/index.js which forces a single resize 
 on load to make sure that this works wit SSR and builds on gatsby-cloud. */
 
-// export default function useResizableWindow() {
-//   const [dimensions, setDimensions] = useState({
-//     width: typeof window !== 'undefined' ? window.innerWidth : 0,
-//     height: typeof window !== 'undefined' ? window.innerHeight : 0
-//   });
-//   const handleResize = () => {
-//     setDimensions({
-//       width: window.innerWidth,
-//       height: window.innerHeight
-//     })
-//   };
-//   useEffect(() => {
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
+export default function useResizableWindow() {
+  const [dimensions, setDimensions] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0
+  });
+  const handleResize = () => {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  };
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-//   return dimensions;
-// };
+  return dimensions;
+};
