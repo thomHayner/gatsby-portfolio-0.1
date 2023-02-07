75163b1bf2269235fcd7435dea934e9fa81afd50
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 02:24:06 -0700

    edit layout and style

--- src/components/portfolio/ServicesPamphlet.js
+++ src/components/portfolio/ServicesPamphlet.js
@@ -6,24 +6,24 @@ export default function ServicesPamphlet() {
   return (
     <Container>
       <Row>
-        <Col className="d-flex justify-content-center" >
-          <h1 className="text-lightest-slate" >{contentData.title}</h1> 
+        <Col className='d-flex justify-content-center' >
+          <h1 className='text-lightest-slate' >{contentData.title}</h1> 
         </Col>
       </Row>
       <Row>
-        <Col className="d-flex justify-content-center" >
-          <h5 className="text-light-slate" >{contentData.subTitle}</h5>
+        <Col className='d-flex justify-content-center' >
+          <h3 className='text-light-slate' >{contentData.subTitle}</h3>
         </Col>
       </Row>
       <Row>
-        <CardGroup className="d-flex flex-wrap my-4" >
+        <CardGroup className='d-flex flex-wrap my-4' >
           {contentData.services.map(service => (
-            <Col key={`Services_${service.id}`} className="col-12 col-md-6 col-lg-4 p-2">
-              <Card className="p-3 h-100 bg-primary border border-green shadow-lg">
-                <Card.Title className="text-lightest-slate" >
+            <Col key={`Services_${service.id}`} className='col-12 col-md-6 col-lg-4 p-2'>
+              <Card className='p-3 h-100 bg-dark-navy border border-green-tetrad border-opacity-75 rounded shadow-lg'>
+                <Card.Title className='text-lightest-slate' >
                   {service.title}
                 </Card.Title>
-                <Card.Body className="p-0 text-light-slate" >
+                <Card.Body className='p-0 text-light-slate' >
                   {service.text}
                 </Card.Body>
               </Card>
