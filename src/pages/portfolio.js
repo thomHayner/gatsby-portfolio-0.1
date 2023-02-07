40dbfa3bf1c7bba57c5a0546711876ef45e3ede0
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 02:31:27 -0700

    add Certifications section, layout, cleanup

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,54 +1,38 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Layout from '../components/layout';
-// import Seo from '../components/seo';
 import TechStackPamphlet from '../components/portfolio/TechStackPamphlet';
 import ServicesPamphlet from '../components/portfolio/ServicesPamphlet';
+import CertificationsPamphlet from '../components/portfolio/CertificationsPamphlet'
 // import ProjectPamphlet from '../components/portfolio/ProjectPamphlet';
-import useResizableWindow from '../hooks/useResizableWindow';
-
-//// DEVNOTE:
-/* The last <Row /> element on this page has it's height shortened by an 
-additional 55px to account for the bottom navbar/footer. */
+// import useResizableWindow from '../hooks/useResizableWindow';
 
 export default function PortfolioPage() {
-  const dimensions = useResizableWindow();
-  const viewHeight = dimensions.height - 52;
+  // const dimensions = useResizableWindow();
+  // const viewHeight = dimensions.height - 52;
 
-  React.useEffect(()=> {
-    if (typeof window !== 'undefined') {
-      window.resizeTo(1366, 768);
-    };
-  }, [])
+  // React.useEffect(()=> {
+  //   if (typeof window !== 'undefined') {
+  //     window.resizeTo(1366, 768);
+  //   };
+  // }, [])
 
   return (
     <Layout>
       <Container id='services'>
-        <Row
-          style={{ minHeight: viewHeight }}
-          className='align-content-center py-3'
-        >
+        <Row className='align-content-center my-5 py-3'>
           <ServicesPamphlet />
         </Row>
-        <Row><hr className='m-0' /></Row>
-        <Row
-          style={{ minHeight: viewHeight - 55 }}
-          className='align-content-center py-3'
-        >
+        <Row><hr className='my-0' /></Row>
+        <Row className='align-content-center my-5 py-3'>
           <TechStackPamphlet />
         </Row>
+        <Row><hr className='my-0' /></Row>
+        <Row className='align-content-center my-5 py-3'>
+          <CertificationsPamphlet />
+        </Row>
         {/* <Row><hr className='m-0' /></Row> */}
-        {/* <Row
-          style={{ minHeight: viewHeight }}
-          className='align-content-center py-3'
-        >
-          {`certifications go here`}
-        </Row> */}
-        {/* <Row><hr className='m-0' /></Row> */}
-        {/* <Row
-          style={{ minHeight: viewHeight }}
-          className='align-content-center py-3'
-        >
+        {/* <Row className='align-content-center my-5 py-3'>
           <ProjectPamphlet />
         </Row> */}
       </Container>
@@ -60,6 +44,8 @@ export default function PortfolioPage() {
 /* This was moved into src/pages/index.js, the main entry point for this page, 
 so that it would populate a browser's tab with a title.  Do some more research 
 to learn about dublication / de-duplication in these scenarios. */
+
+// import Seo from '../components/seo';
 
 // export const Head = () => (
 //   <Seo title='Portfolio' />
