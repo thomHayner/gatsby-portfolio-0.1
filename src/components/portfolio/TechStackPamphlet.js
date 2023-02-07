aebfa884926e746d987dbe7858172564dd0116db
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 02:24:12 -0700

    edit layout and style

--- src/components/portfolio/TechStackPamphlet.js
+++ src/components/portfolio/TechStackPamphlet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import contentData from '../../assets/content/tech-stack-pamphlet.json';
 import { DevIcons } from '../../assets/images/techStackIcons';
 
@@ -7,27 +7,35 @@ export default function TechStackPamphlet() {
   return (
     <Container>
       <Row>
-        <Col className="d-flex justify-content-center" >
-          <h1 className="text-lightest-slate" >{contentData.title}</h1>
+        <Col className='d-flex justify-content-center' >
+          <h1 className='text-lightest-slate' >{contentData.title}</h1>
         </Col>
       </Row>
       <Row>
-        <Col className="d-flex justify-content-center" >
-          <h5 className="text-light-slate" >{contentData.subTitle}</h5>
+        <Col className='d-flex justify-content-center' >
+          <h3 className='text-light-slate' >{contentData.subTitle}</h3>
         </Col>
       </Row>
       <Row>
-        <Col className="col-12 col-lg-3 col-xl-4 my-auto" >
+        <Col className='col-12 col-lg-3 col-xl-4 my-auto' >
           {contentData.techBulletPoint.map(bulletPoint => (
-            <Row key={"bulletPoint_" + bulletPoint.id} className="m-3 pt-2 border-blue border rounded" >
-              <h5 className="text-lightest-slate" >{bulletPoint.title}</h5>
-              <p className="text-light-slate" >{bulletPoint.text}</p>
-            </Row>
+            <Card key={'bulletPoint_' + bulletPoint.id} className='m-3 pt-2 bg-dark-navy border border-blue-tetrad border-opacity-75 rounded'>
+              <Card.Title className='text-lightest-slate' >
+                {bulletPoint.title}
+              </Card.Title>
+              <Card.Body className='text-light-slate' >
+                {bulletPoint.text}
+              </Card.Body>
+            </Card>
           ))}
         </Col>
-        <Col className="col-12 col-lg-9 col-xl-8 d-flex justify-content-center flex-wrap bg-secondary border-tertiary rounded" >
+        <Col
+          className='col-12 col-lg-9 col-xl-8 
+            d-flex flex-wrap justify-content-center 
+            bg-secondary  border border-orange-tetrad border-opacity-75 rounded'
+        >
           {DevIcons.map((DevIcon, i) => (
-            <DevIcon key={"devIcon_" + i} className="m-md-3 m-sm-2" style={{ height: "75px", width: "75px" }} />
+            <DevIcon key={'devIcon_' + i} className='m-md-3 m-sm-2' style={{ height: '75px', width: '75px' }} />
           ))}
         </Col>
       </Row>
