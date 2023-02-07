aed3604cef3f0a5aa4c90d6777224ae875b82777
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 02:24:00 -0700

    edit layout and style

--- src/components/portfolio/CertificationsPamphlet.js
+++ src/components/portfolio/CertificationsPamphlet.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Col, Row, Nav, Tab } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import HackReactor from '../../assets/images/HackReactorLogo.png';
 import contentData from '../../assets/content/certifications-pamphlet.json';
 
@@ -7,32 +7,32 @@ export default function CertificationsPamphlet() {
   return (
     <Container>
       <Row>
-        <Col className="d-flex justify-content-center" >
+        <Col className='d-flex justify-content-center' >
           <h1>{contentData.title}</h1>
         </Col>
       </Row>
       <Row>
-        <Col className="d-flex justify-content-center mb-3" >
-          <h5>{contentData.subTitle}</h5>
+        <Col className='d-flex justify-content-center' >
+          <h3>{contentData.subTitle}</h3>
         </Col>
       </Row>
-      {contentData.certifications.map((cert) => (
-        <Container className='d-inline-flex bg-seconday p-3 rounded border border-pink'>
+      {contentData.certifications.map((certificate) => (
+        <div className='p-3 d-inline-flex bg-dark-navy border border-pink-tetrad border-opacity-75 rounded'>
           <div className='m-1 me-2'>
             <img
               className='rounded'
-              alt={cert.img.alt}
+              alt={certificate.img.alt}
               src={HackReactor}
               width='200'
               height='200'
             />
           </div>
           <div className='m-3'>
-            <span className='fs-4 text-lightest-slate'>{cert.certificate}</span><br />
-            <span className='fs-5 text-light-slate'>{cert.organization}</span><br />
-            <span className='fs-6 text-lightest-slate'>{cert.description}</span>
+            <span className='fs-4 text-lightest-slate'>{certificate.certificate}</span><br />
+            <span className='fs-5 text-light-slate'>{certificate.organization}</span><br />
+            <span className='fs-6 text-lightest-slate'>{certificate.description}</span>
           </div>
-        </Container>
+        </div>
       ))}
     </Container>
   )
