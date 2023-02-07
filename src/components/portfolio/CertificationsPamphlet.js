b296e772d0779fbe68d0988c3cf617d20d6d1be6
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 05:01:18 -0700

    add key to mapped items

--- src/components/portfolio/CertificationsPamphlet.js
+++ src/components/portfolio/CertificationsPamphlet.js
@@ -17,13 +17,16 @@ export default function CertificationsPamphlet() {
         </Col>
       </Row>
       <CardGroup className='px-3'>
-        {contentData.certifications.map((cert) => (
-          <Card className='p-2 pe-md-4 d-inline-flex bg-dark-navy border border-pink-tetrad border-opacity-75 rounded'>
+        {contentData.certifications.map((certificate) => (
+          <Card
+            key={'certification_' + certificate.id}
+            className='p-2 pe-md-4 d-inline-flex bg-dark-navy border border-pink-tetrad border-opacity-75 rounded'
+          >
             <Row className='g-0'>
               <Col className='col-12 col-md-4 col-lg-3 d-flex align-items-center justify-content-center justify-content-md-start'>
                 <img
                   className='rounded'
-                  alt={cert.img.alt}
+                  alt={certificate.img.alt}
                   src={HackReactor}
                   width='200'
                   height='194.5'
@@ -31,10 +34,10 @@ export default function CertificationsPamphlet() {
               </Col>
               <Col className='col-12 col-md-8 col-lg-9 mt-4 my-md-auto justify-content-center justify-content-md-start'>
                 <Card.Title className='text-center text-md-start'>
-                  <h4 className='text-lightest-slate mb-1'>{cert.certificate}</h4>
-                  <h5 className='text-light-slate'>{cert.organization}</h5>
+                  <h4 className='text-lightest-slate mb-1'>{certificate.certificate}</h4>
+                  <h5 className='text-light-slate'>{certificate.organization}</h5>
                 </Card.Title>
-                <Card.Body className='p-md-0 m-0 mt-3 fs-6 text-lightest-slate'>{cert.description}</Card.Body>
+                <Card.Body className='p-md-0 m-0 mt-3 fs-6 text-lightest-slate'>{certificate.description}</Card.Body>
               </Col>
             </Row>
           </Card>
