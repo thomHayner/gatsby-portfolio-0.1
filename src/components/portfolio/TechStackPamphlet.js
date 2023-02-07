3ebd7e076cfd7349ecb52e23a59ae996979dbaeb
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 04:27:55 -0700

    fix card styles and padding, cleanup

--- src/components/portfolio/TechStackPamphlet.js
+++ src/components/portfolio/TechStackPamphlet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, CardGroup, Card } from 'react-bootstrap';
 import contentData from '../../assets/content/tech-stack-pamphlet.json';
 import { DevIcons } from '../../assets/images/techStackIcons';
 
@@ -16,26 +16,28 @@ export default function TechStackPamphlet() {
           <h3 className='text-light-slate' >{contentData.subTitle}</h3>
         </Col>
       </Row>
-      <Row>
-        <Col className='col-12 col-lg-3 col-xl-4 my-auto' >
-          {contentData.techBulletPoint.map(bulletPoint => (
-            <Card key={'bulletPoint_' + bulletPoint.id} className='m-3 pt-2 bg-dark-navy border border-blue-tetrad border-opacity-75 rounded'>
-              <Card.Title className='text-lightest-slate' >
-                {bulletPoint.title}
-              </Card.Title>
-              <Card.Body className='text-light-slate' >
-                {bulletPoint.text}
-              </Card.Body>
-            </Card>
-          ))}
+      <Row className='px-4'>
+        <Col className='col-12 col-lg-4 my-auto' >
+          {/* <CardGroup className=''> */}
+            {contentData.techBulletPoint.map(bulletPoint => (
+              <Card key={'bulletPoint_' + bulletPoint.id} className='m-3 p-3 bg-dark-navy border border-blue-tetrad border-opacity-75 rounded'>
+                <Card.Title className='text-lightest-slate' >
+                  {bulletPoint.title}
+                </Card.Title>
+                <Card.Body className='p-0 text-light-slate' >
+                  {bulletPoint.text}
+                </Card.Body>
+              </Card>
+            ))}
+          {/* </CardGroup> */}
         </Col>
         <Col
-          className='col-12 col-lg-9 col-xl-8 
+          className='col-12 col-lg-8 
             d-flex flex-wrap justify-content-center 
             bg-secondary  border border-orange-tetrad border-opacity-75 rounded'
         >
           {DevIcons.map((DevIcon, i) => (
-            <DevIcon key={'devIcon_' + i} className='m-md-3 m-sm-2' style={{ height: '75px', width: '75px' }} />
+            <DevIcon key={'devIcon_' + i} className='m-1 m-sm-2 m-md-3' style={{ height: '75px', width: '75px' }} />
           ))}
         </Col>
       </Row>
