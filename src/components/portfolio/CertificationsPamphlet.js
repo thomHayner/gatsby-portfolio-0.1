0595f1a6e0245605fc8614dfec6d7acb9a01d68f
Thomas Hayner <thom.hayner@gmail.com>
2023-02-07 04:29:23 -0700

    fix card styles and padding, cleanup

--- src/components/portfolio/CertificationsPamphlet.js
+++ src/components/portfolio/CertificationsPamphlet.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, CardGroup, Card } from 'react-bootstrap';
 import HackReactor from '../../assets/images/HackReactorLogo.png';
 import contentData from '../../assets/content/certifications-pamphlet.json';
 
@@ -7,33 +7,39 @@ export default function CertificationsPamphlet() {
   return (
     <Container>
       <Row>
-        <Col className='d-flex justify-content-center' >
+        <Col className="d-flex justify-content-center" >
           <h1>{contentData.title}</h1>
         </Col>
       </Row>
       <Row>
-        <Col className='d-flex justify-content-center' >
+        <Col className="d-flex justify-content-center" >
           <h3>{contentData.subTitle}</h3>
         </Col>
       </Row>
-      {contentData.certifications.map((certificate) => (
-        <div className='p-3 d-inline-flex bg-dark-navy border border-pink-tetrad border-opacity-75 rounded'>
-          <div className='m-1 me-2'>
-            <img
-              className='rounded'
-              alt={certificate.img.alt}
-              src={HackReactor}
-              width='200'
-              height='200'
-            />
-          </div>
-          <div className='m-3'>
-            <span className='fs-4 text-lightest-slate'>{certificate.certificate}</span><br />
-            <span className='fs-5 text-light-slate'>{certificate.organization}</span><br />
-            <span className='fs-6 text-lightest-slate'>{certificate.description}</span>
-          </div>
-        </div>
-      ))}
+      <CardGroup>
+        {contentData.certifications.map((cert) => (
+          <Card className='p-2 pe-md-4 d-inline-flex bg-dark-navy border border-pink-tetrad border-opacity-75 rounded'>
+            <Row className='g-0'>
+              <Col className='col-12 col-md-4 col-lg-3 d-flex align-items-center justify-content-center justify-content-md-start'>
+                <img
+                  className='rounded'
+                  alt={cert.img.alt}
+                  src={HackReactor}
+                  width='200'
+                  height='200'
+                />
+              </Col>
+              <Col className='col-12 col-md-8 col-lg-9 mt-4 my-md-auto justify-content-center justify-content-md-start'>
+                <Card.Title className='text-center text-md-start'>
+                  <h4 className='text-lightest-slate mb-1'>{cert.certificate}</h4>
+                  <h5 className='text-light-slate'>{cert.organization}</h5>
+                </Card.Title>
+                <Card.Body className='p-md-0 m-0 mt-3 fs-6 text-lightest-slate'>{cert.description}</Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        ))}
+      </CardGroup>
     </Container>
   )
 };
