1da5c70e45cb84fedc84a26127869db04c604eb1
Thomas Hayner <thom.hayner@gmail.com>
2023-02-08 02:51:54 -0700

    comments

--- src/hooks/useResizableWindow.js
+++ src/hooks/useResizableWindow.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { window } from 'browser-monads';
 
+//// DEVNOTE: https://gist.github.com/joshcawthorne/0a518b164658510f4eed74d0c4e8d003
+//// DEVNOTE: https://github.com/Jense5/browser-monads
+
 export default function useResizableWindow() {
   const [dimensions, setDimensions] = useState({
     width: undefined,
@@ -21,31 +24,3 @@ export default function useResizableWindow() {
 
   return dimensions;
 };
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-// //// DEVNOTE:
-// /* There is a useEffect in src/pages/index.js which forces a single resize 
-// on load to make sure that this works wit SSR and builds on gatsby-cloud. */
-
-// export default function useResizableWindow() {
-//   const [dimensions, setDimensions] = useState({
-//     width: typeof window !== 'undefined' ? window.innerWidth : 0,
-//     height: typeof window !== 'undefined' ? window.innerHeight : 0
-//   });
-//   const handleResize = () => {
-//     setDimensions({
-//       width: window.innerWidth,
-//       height: window.innerHeight
-//     })
-//   };
-//   useEffect(() => {
-//     window.addEventListener('resize', handleResize);
-//     handleResize();
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   return dimensions;
-// };
