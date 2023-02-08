e230cdac6515313c14ca3bcf5dd69ec9d26959dd
Thomas Hayner <thom.hayner@gmail.com>
2023-02-08 04:16:26 -0700

    init privacy policy icon

---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Nav } from 'react-bootstrap';
+import PrivacyIcon from '../assets/icons/shield-check.svg';
+
+export default function PrivacyPolicy() {
+  return (
+    <a
+      href='https://www.freeprivacypolicy.com/live/4f116e35-bbbe-47ca-927c-0dcd3495881a'
+      target='_blank'
+      rel='noreferrer'
+      className='nav-link'
+      aria-label='Privacy Policy'
+    >
+      <PrivacyIcon />
+    </a>
+  )
+};
